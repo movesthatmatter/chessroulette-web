97a559bf70d26453b41cf9eb3cbd4fb8a91c749b
Andrei Topli <andrei.topli@gmail.com>
2020-04-24 19:40:27 +0300

    css button update

--- src/components/LandingPage/CircularButton/CircularButton.tsx
+++ src/components/LandingPage/CircularButton/CircularButton.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CircularButton.css';
 
 
-export default function CircularButton(props : {imageURL : string , color : string}) {
-    return <div className="button"
-                style={{background : props.color}}>
+export default function CircularButton (props : {
+    imageURL : string , 
+    color : string, 
+    }) {
+    const [mouseOver, setMouseOver] = useState<boolean | undefined>(false);
+    let btnStyle : Array<string> = ["button"];
+    if (mouseOver) {
+        btnStyle.push("mouseOver");
+    }
+    return <div className={btnStyle.join(' ')}
+                style={{background : props.color}}
+                onMouseOver={()=> {
+                    setMouseOver(true);
+                }}
+                onMouseOut={()=> {
+                    setMouseOver(false);
+                }}>
         <img src ={props.imageURL} alt="buttonLabel"/>
     </div>
 }
