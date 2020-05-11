61e23e90acd9439ba5957cc850c735db3df8e457
Gabriel C. Troia <catalin.troia@gmail.com>
2020-05-11 14:16:44 +0300

    feat(components/mutunachi): add the crazy dudes

---
+++ src/components/Mutunachi/Mutunachi.story.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { Mutunachi } from './Mutunachi';
+
+
+export default {
+  component: Mutunachi,
+  title: 'Components/Mutunachi',
+};
+
+export const defaultStory = () => (
+  <>
+    <Mutunachi mid={0} style={{ width: '150px' }} />
+    <Mutunachi mid={1} style={{ width: '150px' }} />
+    <Mutunachi mid={2} style={{ width: '150px' }} />
+    <Mutunachi mid={3} style={{ width: '150px' }} />
+    <Mutunachi mid={4} style={{ width: '150px' }} />
+    <Mutunachi mid={5} style={{ width: '150px' }} />
+    <Mutunachi mid={6} style={{ width: '150px' }} />
+    <Mutunachi mid={7} style={{ width: '150px' }} />
+    <Mutunachi mid={8} style={{ width: '150px' }} />
+    <Mutunachi mid={9} style={{ width: '150px' }} />
+    <Mutunachi mid={10} style={{ width: '150px' }} />
+  </>
+);
---
+++ src/components/Mutunachi/Mutunachi.tsx
@@ -0,0 +1,37 @@
+import React, { HTMLProps } from 'react';
+import m0 from './assets/mutunachi_0.png';
+import m1 from './assets/mutunachi_1.png';
+import m2 from './assets/mutunachi_2.png';
+import m3 from './assets/mutunachi_3.png';
+import m4 from './assets/mutunachi_4.png';
+import m5 from './assets/mutunachi_5.png';
+import m6 from './assets/mutunachi_6.png';
+import m7 from './assets/mutunachi_7.png';
+import m8 from './assets/mutunachi_8.png';
+import m9 from './assets/mutunachi_9.png';
+import m10 from './assets/mutunachi_10.png';
+
+const map = {
+  0: m0,
+  1: m1,
+  2: m2,
+  3: m3,
+  4: m4,
+  5: m5,
+  6: m6,
+  7: m7,
+  8: m8,
+  9: m9,
+  10: m10,
+};
+
+type Props = React.DetailedHTMLProps<
+React.ImgHTMLAttributes<HTMLImageElement>,
+HTMLImageElement
+> & {
+  mid: keyof typeof map;
+};
+
+export const Mutunachi: React.FC<Props> = ({ mid, ...imgProps }) => (
+  <img src={map[mid]} alt="Mutunachi" {...imgProps} />
+);
