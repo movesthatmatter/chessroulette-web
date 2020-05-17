97ed3d0048663551f095c3fda4f31b95cd0c3baf
Gabriel C. Troia <catalin.troia@gmail.com>
2020-05-17 20:55:13 +0300

    fix(mutunachi): fix the mutunachi once and for all by removing the div and displaying as block

--- src/components/Mutunachi/Mutunachi.tsx
+++ src/components/Mutunachi/Mutunachi.tsx
@@ -10,8 +10,14 @@ import m7 from './assets/mutunachi_7.png';
 import m8 from './assets/mutunachi_8.png';
 import m9 from './assets/mutunachi_9.png';
 import m10 from './assets/mutunachi_10.png';
-import m11 from './assets/Group 20.png';
+import m11 from './assets/mutunachi_11.png';
 import m12 from './assets/mutunachi_ice_cream_and_baloons.png';
+import m13 from './assets/mutunachi_13.png';
+import m14 from './assets/mutunachi_14.png';
+import m15 from './assets/mutunachi_15.png';
+import m16 from './assets/mutunachi_16.png';
+import m17 from './assets/mutunachi_17.png';
+import m18 from './assets/mutunachi_18.png';
 
 const map = {
   0: m0,
@@ -27,6 +33,12 @@ const map = {
   10: m10,
   11: m11,
   12: m12,
+  13: m13,
+  14: m14,
+  15: m15,
+  16: m16,
+  17: m17,
+  18: m18,
 };
 
 export type MutunachiProps = React.DetailedHTMLProps<
@@ -34,20 +46,23 @@ React.ImgHTMLAttributes<HTMLImageElement>,
 HTMLImageElement
 > & {
   mid: keyof typeof map;
+  imgStyle?: React.ImgHTMLAttributes<HTMLImageElement>['style'];
 };
 
 export const Mutunachi: React.FC<MutunachiProps> = ({
-  mid, className, style, height, ...imgProps
+  mid, className, style, ...imgProps
 }) => (
-  <div className={className} style={style}>
-    <img
-      src={map[mid]}
-      alt="Mutunachi"
-      {...imgProps}
-      style={{
-        height: height ? `${height}` : '100%',
-        objectFit: 'contain',
-      }}
-    />
-  </div>
+  <img
+    className={className}
+    src={map[mid]}
+    alt="Mutunachi"
+    style={{
+      objectFit: 'contain',
+      width: '100%',
+      display: 'block',
+
+      ...style,
+    }}
+    {...imgProps}
+  />
 );
