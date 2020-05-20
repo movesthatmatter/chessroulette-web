903160f5f04ff98c8325fb6ae6b605ded8625968
Gabriel C. Troia <catalin.troia@gmail.com>
2020-05-20 16:46:19 +0300

    feat(config): show the whole config in DEBUG

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 
-const DEBUG = !!process.env.REACT_APP_DEBUG;
+const DEBUG = process.env.REACT_APP_DEBUG === 'true';
 const ENV = process.env.REACT_APP_ENV as 'dev' | 'production' | 'staging';
 
 const WSS_ENDPOINT = process.env.REACT_APP_WSS_ENDPOINT as string;
@@ -7,6 +7,7 @@ const HTTP_ENDPOINT = process.env.REACT_APP_HTTP_ENDPOINT as string;
 const REACT_APP_ICE_SERVERS = JSON.parse(process.env.REACT_APP_ICE_SERVERS as string) as string[];
 
 const SENTRY_DSN = process.env.REACT_APP_SENTRY_DSN as string;
+const GOOGLE_ANALYTICS_TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID as string;
 
 const config = {
   // Env
@@ -20,6 +21,12 @@ const config = {
 
   // Vendors
   SENTRY_DSN,
+  GOOGLE_ANALYTICS_TRACKING_ID,
 };
+
+if (config.DEBUG) {
+  // eslint-disable-next-line no-console
+  console.log('App Config', config);
+}
 
 export default config;
