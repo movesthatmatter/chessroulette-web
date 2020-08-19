ba363ebb3d32f26cf76b4b0f1170351892244bec
Gabriel C. Troia <catalin.troia@gmail.com>
2020-08-19 17:15:00 -0600

    Fixx src/lib/jss types

--- src/lib/jss/createUseStyles.ts
+++ src/lib/jss/createUseStyles.ts
@@ -1,9 +1,13 @@
 import { createUseStyles as createUseStylesJSS, DefaultTheme } from 'react-jss';
+import {
+  Styles,
+} from 'jss';
 import { CSSProperties } from './types';
 
 // Add stricter types for this function
 export function createUseStyles<Theme = DefaultTheme, C extends string = string>(
-  styles: Record<C, CSSProperties> | ((theme: Theme) => Record<C, unknown>),
+  // styles: Record<C, CSSProperties> | ((theme: Theme) => Record<C, unknown>),
+  styles: Styles<C> | ((theme: Theme) => Styles<C>),
   options?: {},
 ) {
   return createUseStylesJSS(styles, options);
