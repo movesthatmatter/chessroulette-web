cec198149dd92a2ba5266efd18933f8930010a6a
Gabriel C. Troia <catalin.troia@gmail.com>
2020-08-22 14:15:04 -0600

    Fix the jss issue

--- src/lib/jss/createUseStyles.ts
+++ src/lib/jss/createUseStyles.ts
@@ -6,9 +6,8 @@ import { CSSProperties } from './types';
 
 // Add stricter types for this function
 export function createUseStyles<Theme = DefaultTheme, C extends string = string>(
-  // styles: Record<C, CSSProperties> | ((theme: Theme) => Record<C, unknown>),
-  styles: Styles<C> | ((theme: Theme) => Styles<C>),
+  styles: Record<C, CSSProperties> | ((theme: Theme) => Record<C, unknown>),
   options?: {},
 ) {
-  return createUseStylesJSS(styles, options);
+  return createUseStylesJSS(styles as Styles<C> | ((theme: Theme) => Styles<C>), options);
 }
