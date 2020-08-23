969db2d661d91b88f4da3e14cb2db462c6391f5f
Gabriel C. Troia <catalin.troia@gmail.com>
2020-08-23 15:44:02 -0600

    GameRoom2: Show a game finished popup

--- src/modules/GameRoomV2/GameRoomV2.story.tsx
+++ src/modules/GameRoomV2/GameRoomV2.story.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grommet } from 'grommet';
 import { WithLocalStream } from 'src/storybook/WithLocalStream';
 import { defaultTheme } from 'src/theme';
 import { PeerMocker } from 'src/mocks/records/PeerMocker';
-import { reduceChessGame, ChessGameState } from 'src/modules/Games/Chess';
+import { reduceChessGame, ChessGameState, ChessGameColor } from 'src/modules/Games/Chess';
 import { Page } from 'src/components/Page/Page';
 import { GameRoomV2 } from './GameRoomV2';
+import { otherChessColor } from '../Games/Chess/util';
 
 export default {
   component: GameRoomV2,
@@ -48,6 +49,7 @@ export const defaultStory = () => (
           me={me}
           opponent={opponent}
           game={currentGame}
+          onGameStateUpdate={() => currentGame}
         />
       );
     })}
@@ -74,13 +76,13 @@ export const asPage = () => (
         },
       });
 
-      const [currentGame] = useState<ChessGameState>(reduceChessGame.prepareGame({
+      const [currentGame, setCurrentGame] = useState<ChessGameState>(reduceChessGame.prepareGame({
         playersBySide: {
           away: opponent,
           home: me,
         },
-        homeColor: 'random',
-        timeLimit: 'blitz',
+        homeColor: 'white',
+        timeLimit: 'bullet',
       }));
 
       return (
@@ -89,6 +91,104 @@ export const asPage = () => (
             me={me}
             opponent={opponent}
             game={currentGame}
+            onGameStateUpdate={setCurrentGame}
+          />
+        </Page>
+      );
+    })}
+    />
+  </Grommet>
+);
+
+export const asPageWithSwitchingSides = () => (
+  <Grommet theme={defaultTheme} full>
+    <WithLocalStream render={(stream) => React.createElement(() => {
+      const [playersBySide, setPlayersBySide] = useState({
+        home: peerMock.withChannels({
+          streaming: {
+            on: true,
+            type: 'audio-video',
+            stream,
+          },
+        }),
+        away: peerMock.withChannels({
+          streaming: {
+            on: true,
+            type: 'audio-video',
+            stream,
+          },
+        }),
+      });
+
+      const [currentGame, setCurrentGame] = useState<ChessGameState>(reduceChessGame.prepareGame({
+        playersBySide,
+        homeColor: 'white',
+        timeLimit: 'bullet',
+      }));
+
+      // useEffect(() => {
+      //   setCu
+      // }, [homeColor]);
+
+      return (
+        <Page>
+          <GameRoomV2
+            me={playersBySide.home}
+            opponent={playersBySide.away}
+            game={currentGame}
+            // onGameStateUpdate={setCurrentGame}
+            onGameStateUpdate={(nextGame) => {
+              setPlayersBySide((prev) => ({
+                home: prev.away,
+                away: prev.home,
+              }));
+
+              setCurrentGame(nextGame);
+            }}
+          />
+        </Page>
+      );
+    })}
+    />
+  </Grommet>
+);
+
+export const asPageWithFinishedGame = () => (
+  <Grommet theme={defaultTheme} full>
+    <WithLocalStream render={(stream) => React.createElement(() => {
+      const me = peerMock.withChannels({
+        streaming: {
+          on: true,
+          type: 'audio-video',
+          stream,
+        },
+      });
+
+      const opponent = peerMock.withChannels({
+        streaming: {
+          on: true,
+          type: 'audio-video',
+          stream,
+        },
+      });
+
+      const [currentGame, setCurrentGame] = useState<ChessGameState>(reduceChessGame.prepareGame({
+        playersBySide: {
+          away: opponent,
+          home: me,
+        },
+        homeColor: 'white',
+        timeLimit: 'bullet',
+        pgn: '1. e4 e5 2. Qf3 Na6 3. Bc4 h6 4. Qxf7#',
+      }));
+
+      return (
+        <Page>
+          <GameRoomV2
+            me={me}
+            opponent={opponent}
+            game={currentGame}
+            onGameStateUpdate={setCurrentGame}
           />
         </Page>
       );
--- src/modules/GameRoomV2/GameRoomV2.tsx
+++ src/modules/GameRoomV2/GameRoomV2.tsx
@@ -1,29 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createUseStyles } from 'src/lib/jss';
 import { StreamingBox } from 'src/components/StreamingBox';
 import { Peer } from 'src/components/RoomProvider';
-import { ChessGameV2 } from 'src/modules/Games/Chess/components/ChessGameV2';
-import { ChessGameState } from 'src/modules/Games/Chess';
+import { StandaloneChessGame } from 'src/modules/Games/Chess/components/StandaloneChessGame';
+import { ChessGameState, reduceChessGame } from 'src/modules/Games/Chess';
+import { Box } from 'grommet';
+import { PopupContent } from 'src/components/PopupContent';
+import { Modal } from 'src/components/Modal/Modal';
 import { GameRoomLayout } from './GameRoomLayout/GameRoomLayout';
 
 type Props = {
   me: Peer;
   opponent: Peer;
   game: ChessGameState | undefined;
+  onGameStateUpdate: (nextGame: ChessGameState) => void;
 };
 
 export const GameRoomV2: React.FC<Props> = (props) => {
   const cls = useStyles();
+  const [showGameFinishedPopup, setShowGameFinishedPopup] = useState(false);
+
+  const homeColor = props.me.id === props.game?.players.black.id ? 'black' : 'white';
+
+  useEffect(() => {
+    if (props.game?.state === 'finished') {
+      setShowGameFinishedPopup(true);
+    }
+  }, [props.game]);
 
   return (
     <div className={cls.container}>
       <GameRoomLayout
         getGameComponent={(dimensions) => (
-          <ChessGameV2
-            homeColor={props.me.id === props.game?.players.black.id ? 'black' : 'white'}
-            playable
+          <StandaloneChessGame
+            homeColor={homeColor}
+            playable={
+              (props.game?.state === 'pending' || props.game?.state === 'started')
+              && props.game.players[homeColor].id === props.me.id
+            }
             game={props.game}
             getBoardSize={() => dimensions.width}
+            onMove={(nextPgn) => {
+              // don't move unless the game is pending or started
+              if (
+                !props.game
+                || props.game.state === 'finished'
+                || props.game.state === 'neverStarted'
+              ) {
+                return;
+              }
+
+              props.onGameStateUpdate(reduceChessGame.move(props.game, { pgn: nextPgn }));
+            }}
+            onTimerFinished={() => {
+              if (
+                !props.game
+                || props.game.state === 'finished'
+                || props.game.state === 'neverStarted'
+              ) {
+                return;
+              }
+
+              props.onGameStateUpdate(reduceChessGame.timerFinished(props.game));
+            }}
           />
         )}
         getStreamingBoxComponent={(dimensions) => (
@@ -34,6 +73,20 @@ export const GameRoomV2: React.FC<Props> = (props) => {
           />
         )}
       />
+      <Modal
+        visible={showGameFinishedPopup}
+        onEsc={() => setShowGameFinishedPopup(false)}
+        onClickOutside={() => setShowGameFinishedPopup(false)}
+      >
+        <PopupContent
+          hasCloseButton
+          onClose={() => setShowGameFinishedPopup(false)}
+        >
+          <Box>
+            {`${props.game?.winner} won`}
+          </Box>
+        </PopupContent>
+      </Modal>
     </div>
   );
 };
