e0aff09f9ba149992db846afeeac81c7e129eb4e
Gabriel C. Troia <catalin.troia@gmail.com>
2020-09-02 13:59:04 -0600

    Add a ClicpBoard Copy commponent

---
+++ src/components/CipboardCopy/ClipboardCopy.story.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { Grommet, Box } from 'grommet';
+import { defaultTheme } from 'src/theme';
+import { ClipboardCopy } from './ClipboardCopy';
+
+export default {
+  component: ClipboardCopy,
+  title: 'components/ClipboardCopy',
+};
+
+export const defaultStory = () => (
+  <Grommet theme={defaultTheme} full>
+    <Box width="medium">
+      <ClipboardCopy
+        value="Press on the button to copy me"
+      />
+    </Box>
+  </Grommet>
+);
---
+++ src/components/CipboardCopy/ClipboardCopy.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { createUseStyles } from 'src/lib/jss';
+import { Box, TextInput, Button } from 'grommet';
+import { Clipboard } from 'grommet-icons';
+
+type Props = {
+  value: string;
+  onCopied?: () => void;
+};
+
+export const ClipboardCopy: React.FC<Props> = (props) => {
+  const cls = useStyles();
+
+  return (
+    <Box direction="row" className={cls.container}>
+      <TextInput
+        value={props.value}
+        plain
+        size="small"
+        className={cls.textInput}
+      />
+      <Button
+        icon={<Clipboard color="light-1" />}
+        className={cls.copyButton}
+        size="small"
+        plain
+        focusIndicator
+        color="dark-1"
+        onClick={() => {
+          navigator.clipboard.writeText(props.value);
+        }}
+      />
+    </Box>
+  );
+};
+
+const useStyles = createUseStyles({
+  container: {
+    border: '1px solid grey',
+    borderRadius: '40px',
+    overflow: 'hidden',
+  },
+  textInput: {
+
+  },
+  copyButton: {
+    background: 'grey !important',
+    padding: '0 1em 0 .7em !important',
+    // color: 'white !important',
+
+    '&:active': {
+      opacity: 0.8,
+    },
+
+    '&:focus': {
+      boxShadow: 'none !important',
+    },
+  },
+});
