118bebf239cfb35a00d2a85d3a2b65b00025d1a2
Gabriel C. Troia <catalin.troia@gmail.com>
2020-10-31 17:17:09 -0700

    feat(containerwithdimensions): add useContainerWithDimensiosn hook to optimize the rerender better

---
+++ src/components/ContainerWithDimensions/useContainerDimensions.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { debounce } from 'debounce';
+
+export type ContainerDimensions = {
+  width: number;
+  height: number;
+  updated: boolean;
+};
+
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+export function useContainerDimensions(targetRef: React.RefObject<any>) {
+  const [dimensions, setDimensions] = useState<ContainerDimensions>({ 
+    width: 0,
+    height: 0,
+    updated: false,
+  });
+
+  useEffect(() => {
+    const onResizeHandler = () => {
+      if (targetRef.current) {
+        setDimensions({
+          width: targetRef.current?.offsetWidth,
+          height: targetRef.current?.offsetHeight,
+          updated: true,
+        });
+      }
+    };
+
+    onResizeHandler();
+
+    window.addEventListener('resize', debounce(onResizeHandler, 250));
+
+    return () => {
+      window.removeEventListener('resize', onResizeHandler);
+    };
+  }, [targetRef]);
+
+  return dimensions;
+}
