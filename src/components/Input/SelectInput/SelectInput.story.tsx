61e6f4663e71ff8cdedf3f15ce0151682a7b464a
Gabriel C. Troia <catalin.troia@gmail.com>
2020-11-04 19:12:47 -0700

    feat(selectinput): add SelectInput component

---
+++ src/components/Input/SelectInput/SelectInput.story.tsx
@@ -0,0 +1,32 @@
+import { action } from '@storybook/addon-actions';
+import { Grommet } from 'grommet';
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { defaultTheme } from 'src/theme';
+import { SelectInput } from './SelectInput';
+
+
+export default {
+  component: SelectInput,
+  title: 'components/Inputs/SelectInput',
+};
+
+const options = [
+  { label: 'option 1', value: '1' },
+  { label: 'option 2', value: '2' },
+  { label: 'option 3', value: '3' },
+];
+
+export const defaultStory = () => (
+  <div style={{ width: '300px' }}>
+    <Grommet theme={defaultTheme}>
+      <SelectInput
+        placeholder="Placeholder"
+        // value="option 1"
+        options={options}
+        onChange={action('on change')}
+        onSelect={action('on select')}
+      />
+    </Grommet>
+  </div>
+)
