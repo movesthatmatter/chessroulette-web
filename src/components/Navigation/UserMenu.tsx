6341ef6a9e2aabd579e1c76713dbb3ea70b57c9e
Gabriel C. Troia <catalin.troia@gmail.com>
2020-11-14 19:29:19 -0700

    feat(usermenu): display a Peer connection state next to the User

--- src/components/Navigation/UserMenu.tsx
+++ src/components/Navigation/UserMenu.tsx
@@ -2,35 +2,58 @@ import { Box } from 'grommet';
 import { Text } from 'src/components/Text';
 import React from 'react';
 import { createUseStyles } from 'src/lib/jss';
-import { AuthenticationConsumer } from 'src/services/Authentication';
-import { floatingShadow, fonts } from 'src/theme';
+import { selectAuthentication } from 'src/services/Authentication';
+import { colors, floatingShadow, fonts } from 'src/theme';
 import { Mutunachi } from '../Mutunachi/Mutunachi';
 import { Avatar } from 'src/components/Avatar';
+import { useSelector } from 'react-redux';
+import { selectMyPeer } from '../PeerProvider';
+import { Peer } from '../RoomProvider';
 
 type Props = {};
 
-export const UserMenu: React.FC<Props> = (props) => {
+const getStatusColor = (peer?: Peer) => {
+  if (!peer) {
+    return colors.neutral;
+  }
+
+  if (peer.connection.channels.streaming.on) {
+    return colors.primary;
+  }
+
+  return colors.positive;
+}
+
+export const UserMenu: React.FC<Props> = () => {
   const cls = useStyles();
+  const auth = useSelector(selectAuthentication);
+  const myPeer = useSelector(selectMyPeer);
+
+  // TODO: Maybe change in the future
+  if (auth.authenticationType === 'none') {
+    return null;
+  }
 
   return (
-    <AuthenticationConsumer
-      renderAuthenticated={(user) => (
-        <Box fill className={cls.container} direction="row">
-          <Box fill direction="row">
-            <Avatar
-              // size="medium"
-              className={cls.avatar}
-            >
-              <Mutunachi mid={user.avatarId} />
-            </Avatar>
-            <Box direction="column">
-              <Text className={cls.userNameText}>{user.name}</Text>
-              <Text className={cls.userType}>{user.isGuest ? 'Guest' : ''}</Text>
-            </Box>
-          </Box>
+    <Box fill className={cls.container} direction="row">
+      <Box fill direction="row">
+        <Avatar className={cls.avatar}>
+          <Mutunachi mid={auth.user.avatarId} />
+        </Avatar>
+        <Box direction="column">
+          <Text className={cls.userNameText}>{auth.user.name}</Text>
+          <Text className={cls.userType}>
+            {auth.user.isGuest ? 'Guest' : ''}
+            <div
+              className={cls.dot}
+              style={{
+                backgroundColor: getStatusColor(myPeer),
+              }}
+            />
+          </Text>
         </Box>
-      )}
-    />
+      </Box>
+    </Box>
   );
 };
 
@@ -47,5 +70,14 @@ const useStyles = createUseStyles({
   },
   userType: {
     ...fonts.small1,
-  }
+  },
+  dot: {
+    height: '8px',
+    width: '8px',
+    backgroundColor: colors.neutral,
+    borderRadius: '50%',
+    display: 'inline-block',
+    marginLeft: '4px',
+  },
+  userNameWrapper: {},
 });
