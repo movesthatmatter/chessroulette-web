693547c508f49c2677217a75355b4fc3aa7eefa0
Gabriel C. Troia <catalin.troia@gmail.com>
2020-11-16 18:52:29 -0700

    feat(https): redirect all http requests to https

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,37 @@ import { PeerProvider } from './components/PeerProvider';
 import { SocketProvider } from './components/SocketProvider';
 import config from './config';
 import { GA } from './services/Analytics';
+import HttpsRedirect from 'react-https-redirect';
 
 function App() {
   useEffect(() => {
     GA.init();
-  }, [])
+  }, []);
 
   return (
-    <ReduxProvider>
-      <AuthenticationProvider>
-        <SocketProvider>
-          <PeerProvider>
-            <JssProvider
-              // Prefix the Mounted classes but not the prerendered ones
-              // This is to avoid style conflict between new and stale
-              //  prerendererd classes since those can't be removed (for now)
-              // The idea of prefixing the mounted classes is to decrease the
-              //  initial html size as much as possible!
-              classNamePrefix={config.PRERENDERING ? undefined : 'cr-'}
-              id={{ minify: !config.DEBUG }}
-            >
-              <ThemeProvider theme={defaultTheme}>
-                <Routes />
-              </ThemeProvider>
-            </JssProvider>
-          </PeerProvider>
-        </SocketProvider>
-      </AuthenticationProvider>
-    </ReduxProvider>
+    <HttpsRedirect>
+      <ReduxProvider>
+        <AuthenticationProvider>
+          <SocketProvider>
+            <PeerProvider>
+              <JssProvider
+                // Prefix the Mounted classes but not the prerendered ones
+                // This is to avoid style conflict between new and stale
+                //  prerendererd classes since those can't be removed (for now)
+                // The idea of prefixing the mounted classes is to decrease the
+                //  initial html size as much as possible!
+                classNamePrefix={config.PRERENDERING ? undefined : 'cr-'}
+                id={{ minify: !config.DEBUG }}
+              >
+                <ThemeProvider theme={defaultTheme}>
+                  <Routes />
+                </ThemeProvider>
+              </JssProvider>
+            </PeerProvider>
+          </SocketProvider>
+        </AuthenticationProvider>
+      </ReduxProvider>
+    </HttpsRedirect>
   );
 }
 
