5984bfa63aa976d9dc596518fe830f3dccf7d137
Gabriel C. Troia <catalin.troia@gmail.com>
2020-12-06 10:58:21 -1000

    feat(clipboard component): improve the styling a bit

--- src/components/CipboardCopy/ClipboardCopy.story.tsx
+++ src/components/CipboardCopy/ClipboardCopy.story.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, { useState } from 'react';
 import { Grommet, Box } from 'grommet';
 import { defaultTheme } from 'src/theme';
 import { ClipboardCopy } from './ClipboardCopy';
+import { action } from '@storybook/addon-actions';
 
 export default {
   component: ClipboardCopy,
@@ -14,6 +15,19 @@ export const defaultStory = () => (
     <Box width="large">
       <ClipboardCopy
         value="Press on the button to copy me"
+        onCopied={action('copied')}
+      />
+    </Box>
+  </Grommet>
+);
+
+export const withAutoCopy = () => (
+  <Grommet theme={defaultTheme} full>
+    <Box width="large">
+      <ClipboardCopy
+        value="This should auto copy"
+        autoCopy
+        onCopied={action('copied')}
       />
     </Box>
   </Grommet>
