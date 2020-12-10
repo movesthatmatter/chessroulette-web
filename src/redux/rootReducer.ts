5b58eba6a4582476eb6a2dd7e7a778154e6849ff
Gabriel C. Troia <catalin.troia@gmail.com>
2020-12-10 01:44:56 -1000

    fix(app): fix the imports after the PeerProvider refactoring

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { stateSliceByKey as authentication } from 'src/services/Authentication';
 import { stateSliceByKey as chessStudy } from 'src/modules/ChessStudy/reducer';
-import { stateSliceByKey as joinedRoom } from 'src/components/PeerProvider/reducer';
+import { stateSliceByKey as joinedRoom } from 'src/components/PeerProvider/redux/reducer';
 import { stateSliceByKey as roomBouncer } from 'src/modules/GenericRoom/GenericRoomBouncer/reducer';
 
 const allModuleReducers = {
