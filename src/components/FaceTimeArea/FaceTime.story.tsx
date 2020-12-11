f206d550a0dc4ec0df42e9e7a117615e229b237e
Gabriel C. Troia <catalin.troia@gmail.com>
2020-12-10 17:49:50 -1000

    feat(facetime): add ability to add overlays - header, footer and main

--- src/components/FaceTimeArea/FaceTime.story.tsx
+++ src/components/FaceTimeArea/FaceTime.story.tsx
@@ -24,3 +24,83 @@ export const defaultStory = () => React.createElement(() => (
     />
   </div>
 ));
+
+export const withOverlays = () => React.createElement(() => (
+  <div style={{ width: '480px' }}>
+    <WithLocalStream
+      render={(stream) => (
+        <FaceTime
+          streamConfig={{
+            on: true,
+            type: 'audio-video',
+            stream,
+          }}
+          aspectRatio={{ width: 16, height: 9 }}
+          muted
+          headerOverlay={(
+            <div>header</div>
+          )}
+          mainOverlay={(
+            <div>
+              main
+            </div>
+          )}
+          footerOverlay={(
+            <div>footer</div>
+          )}
+        />
+      )}
+    />
+  </div>
+));
+
+export const withLabel = () => React.createElement(() => (
+  <div style={{ width: '480px' }}>
+    <WithLocalStream
+      render={(stream) => (
+        <FaceTime
+          streamConfig={{
+            on: true,
+            type: 'audio-video',
+            stream,
+          }}
+          aspectRatio={{ width: 16, height: 9 }}
+          muted
+          label="This is a label"
+          labelPosition="bottom-left"
+        />
+      )}
+    />
+  </div>
+));
+
+export const withLabelAndOverlays = () => React.createElement(() => (
+  <div style={{ width: '480px' }}>
+    <WithLocalStream
+      render={(stream) => (
+        <FaceTime
+          streamConfig={{
+            on: true,
+            type: 'audio-video',
+            stream,
+          }}
+          aspectRatio={{ width: 16, height: 9 }}
+          muted
+          label="This is a label"
+          labelPosition="bottom-left"
+          headerOverlay={(
+            <div>header</div>
+          )}
+          mainOverlay={(
+            <div>
+              main
+            </div>
+          )}
+          footerOverlay={(
+            <div>footer</div>
+          )}
+        />
+      )}
+    />
+  </div>
+));
