d45787b100f308c3b015ddf83565546d11550f62
Gabriel C. Troia <catalin.troia@gmail.com>
2020-12-12 02:18:06 -1000

    refactor(challenge): remove all the previous components for dealing with the challenges

--- src/modules/Challenges/index.ts
+++ src/modules/Challenges/index.ts
@@ -1,1 +1,1 @@
-export * from './ChallengePage';
+export * from './ChallengeOrRoomPage';
--- src/modules/Games/Chess/components/PendingChallenge/index.ts
+++
@@ -1,1 +0,0 @@
-export { PendingChallengeContainer as PendingChallenge } from './PendingChallengeContainer';
