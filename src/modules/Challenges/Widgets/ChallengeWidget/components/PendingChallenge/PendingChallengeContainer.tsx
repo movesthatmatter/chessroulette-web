b55ef2a388ad26b66eca9140b8fb386ce66af6cc
Gabriel C. Troia <catalin.troia@gmail.com>
2020-12-12 21:55:03 -1000

    fix(pendingchallenge): fix an issue where the socket wouldn't get subscribed to the onmessage

--- src/modules/Challenges/Widgets/ChallengeWidget/components/PendingChallenge/PendingChallengeContainer.tsx
+++ src/modules/Challenges/Widgets/ChallengeWidget/components/PendingChallenge/PendingChallengeContainer.tsx
@@ -20,32 +20,34 @@ export type PendingChallengeContainerProps = PendingChallengeProps & {
 
 export const PendingChallengeContainer: React.FC<PendingChallengeContainerProps> = (props) => {
   return (
-    // Note 12/12/2020 Use this for now b/c the useSocketOnMessage hook doesn't 
-    //  register the subscribers correctly! Need to look into that!
-    // What happens is that th useSocketOnMessage wuldnt get the messages smoetimes if
-    //  I believe it subscribes before the socket opens!
-    <SocketConsumer
-      onMessage={(msg) => {
-        if (msg.kind === 'challengeAccepted') {
-          if (props.challenge.type === 'private') {
-            props.onAccepted({
-              id: msg.content.id,
-              chalengeeId: msg.content.userId,
-              room: msg.content.room,
-            });
-          } else {
-            // TODO: This is not 100% true b/c a public challenge could still be
-            //  accepted by going through the link. But for the current purposes
-            //  it's ok. this is a server change where the payload king is different!
-            props.onMatched({
-              id: msg.content.id,
-              chalengeeId: msg.content.userId,
-              room: msg.content.room,
-            });
+    <>
+      {/* // Note 12/12/2020 Use this for now b/c the useSocketOnMessage hook doesn't // register the
+      subscribers correctly! Need to look into that! // What happens is that th useSocketOnMessage
+      wouldnt get the messages smoetimes if // I believe it subscribes before the socket opens! */}
+      <SocketConsumer
+        render={() => null}
+        onMessage={(msg) => {
+          if (msg.kind === 'challengeAccepted') {
+            if (props.challenge.type === 'private') {
+              props.onAccepted({
+                id: msg.content.id,
+                chalengeeId: msg.content.userId,
+                room: msg.content.room,
+              });
+            } else {
+              // TODO: This is not 100% true b/c a public challenge could still be
+              //  accepted by going through the link. But for the current purposes
+              //  it's ok. this is a server change where the payload king is different!
+              props.onMatched({
+                id: msg.content.id,
+                chalengeeId: msg.content.userId,
+                room: msg.content.room,
+              });
+            }
           }
-        }
-      }}
-      render={() => <PendingChallenge challenge={props.challenge} />}
-    />
+        }}
+      />
+      <PendingChallenge challenge={props.challenge} />
+    </>
   );
 };
