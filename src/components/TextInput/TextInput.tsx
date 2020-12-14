80f955b75806adcbe8245922dec1d05ea6f67ccf
Gabriel C. Troia <catalin.troia@gmail.com>
2020-12-13 22:47:29 -1000

    feat(components/textinput): add TextInput component

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createUseStyles, makeImportant } from 'src/lib/jss';
+import { colors } from 'src/theme';
+import { TextInput as GTextInput, TextInputProps } from 'grommet';
+
+type Props = TextInputProps & {
+  className?: string;
+};
+
+export const TextInput: React.FC<Props> = ({
+  size,
+  className,
+  ...props
+}) => {
+  const cls = useStyles();
+
+  return (
+    <div className={cls.container}>
+      <GTextInput value={props.value} plain size="small" className={cls.textInput} />
+    </div>
+  );
+};
+
+const useStyles = createUseStyles({
+  container: {
+    border: `1px solid ${colors.neutral}`,
+    borderRadius: '40px',
+    overflow: 'hidden',
+  },
+  textInput: {
+    ...makeImportant({
+      fontSize: '13px',
+      fontWeight: 'normal',
+      height: '32px',
+    }),
+  },
+});
