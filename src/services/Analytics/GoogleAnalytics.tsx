e609fd440feb2c1b5cd1d6e6cd73d9752a223f75
Gabriel C. Troia <catalin.troia@gmail.com>
2020-12-15 18:38:02 -1000

    fix(google analytics): fix the config, debug flag

--- src/services/Analytics/GoogleAnalytics.tsx
+++ src/services/Analytics/GoogleAnalytics.tsx
@@ -52,7 +52,7 @@ const RouteTracker = () => <Route component={GoogleAnalytics} />;
 const init = (options: InitializeOptions = {}) => {
   const isGAEnabled = config.ENV === 'production' || config.ENV === 'staging';
 
-  if (true || isGAEnabled) {
+  if (isGAEnabled) {
     ReactGA.initialize(config.GOOGLE_ANALYTICS_TRACKING_ID, {
       debug: config.DEBUG,
       ...options,
