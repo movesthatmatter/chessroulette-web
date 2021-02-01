2e2a57d8e80311106055e7cc114eef87343c17dd
Gabriel C. Troia <catalin.troia@gmail.com>
2021-02-01 16:24:00 +0200

    Add a validator lib

--- src/lib/validator/validator.ts
+++ src/lib/validator/validator.ts
@@ -1,9 +1,14 @@
+import { validateEmail } from "./rules";
+
+const rules = {
+  email: () => validateEmail,
+};
+
+const messages: {[k in keyof typeof rules]: string} = {
+  email: 'The email isn\'t valid',
+}
+
 export const validator = {
-  email: {
-    validate: (email: string) => {
-      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(String(email).toLowerCase());
-    },
-    error: 'The email isn\'t valid',
-  },
+  rules,
+  messages,
 };
