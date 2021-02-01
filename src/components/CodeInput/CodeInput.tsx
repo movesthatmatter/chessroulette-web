1eab807472dd40b98586e70c9c7abe0b59041e10
Gabriel C. Troia <catalin.troia@gmail.com>
2021-02-01 23:33:26 +0200

    Add a CodeInput Component for verifying the Email Code

---
+++ src/components/CodeInput/CodeInput.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { createUseStyles, CSSProperties, makeImportant } from 'src/lib/jss';
+import ReactCodeInput from 'react-verification-code-input';
+import { colors, defaultTheme } from 'src/theme';
+import cx from 'classnames';
+
+type Props = Omit<
+  React.ComponentProps<typeof ReactCodeInput>, 'fieldWidth' | 'fieldHeight' | 'fields' | 'type'
+> & {
+  fieldsCount?: number;
+  fieldSize?: number;
+};
+
+export const CodeInput: React.FC<Props> = ({
+  fieldsCount = 5,
+  fieldSize = 60,
+  className,
+  ...restProps
+}) => {
+  const cls = useStyles();
+
+  return (
+    <div className={cx(cls.container, className)}>
+      <ReactCodeInput
+        type="number"
+        fields={fieldsCount}
+        className={cls.codeInput}
+        fieldWidth={fieldSize}
+        fieldHeight={fieldSize}
+        {...restProps}
+      />
+    </div>
+  );
+};
+
+const useStyles = createUseStyles({
+  container: {
+    marginBottom: '16px',
+  },
+  codeInput: {
+    ...makeImportant({
+      width: '100%',
+    }),
+
+    ...{
+      '& > div': {
+        ...makeImportant({
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-between',
+        }),
+      },
+      '& input': {
+        ...makeImportant({
+          fontFamily: defaultTheme.global?.font?.family,
+
+          borderRadius: '8px',
+          border: `1px solid ${colors.neutral}`,
+        }),
+
+        '&:first-child': {
+          marginLeft: '0',
+        },
+
+        '&:focus': {
+          ...makeImportant({
+            borderWidth: '2px',
+            caretColor: colors.neutral,
+          }),
+        }
+      },
+    } as CSSProperties,
+  },
+});
