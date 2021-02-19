d09a6208a483706928ec7e4fa8931c0d0dca217a
Gabriel C. Troia <catalin.troia@gmail.com>
2021-02-20 00:29:31 +0200

    Remove the ChessGame 1st version

--- src/modules/Games/Chess/index.ts
+++ src/modules/Games/Chess/index.ts
@@ -1,5 +1,1 @@
-export * from './components/ChessGame';
-// export * from './records';
-// export * from './types';
-// export { actions as reduceChessGame } from './chessGameStateReducer';
 export * from './useSoundEffects';
