4489329de60a5febd5dd480276ed58f2e13f4453
Gabriel C. Troia <catalin.troia@gmail.com>
2021-03-11 23:42:17 +0200

    feat(authenticatedpage): add AuthenticatedPage Component which handles non auth states

---
+++ src/components/Page/AuthenticatedPage.tsx
@@ -0,0 +1,32 @@
+import { RegisteredUserRecord } from 'dstnd-io';
+import React, { ReactNode } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useAuthenticatedUser } from 'src/services/Authentication';
+import { AuthenticationDialog } from 'src/services/Authentication/widgets';
+import { Page, PageProps } from './Page';
+
+type Props = PageProps & {
+  render?: (p: { user: RegisteredUserRecord }) => ReactNode;
+};
+
+export const AuthenticatedPage: React.FC<Props> = ({
+  render,
+  ...props
+}) => {
+  const user = useAuthenticatedUser();
+  const history = useHistory()
+
+  if (!user || user.isGuest) {
+    return (
+      <AuthenticationDialog visible onClose={() => {
+        history.replace('/');
+      }}/>
+    );
+  }
+
+  const children = render ? render({ user }) : props.children;
+
+  return (
+    <Page {...props} children={children} />
+  );
+};
