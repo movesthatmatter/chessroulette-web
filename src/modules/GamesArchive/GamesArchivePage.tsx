a8f1d51e2cbf046595e7c7ddd5d9887f7522090c
Gabriel C. Troia <catalin.troia@gmail.com>
2021-03-11 23:45:34 +0200

    feat(gamesarchive): start using the AtuhentiatedPage

--- src/modules/GamesArchive/GamesArchivePage.tsx
+++ src/modules/GamesArchive/GamesArchivePage.tsx
@@ -1,47 +1,20 @@
-import capitalize from 'capitalize';
-import { UserRecord } from 'dstnd-io';
 import React, { useEffect, useState } from 'react';
-import { ClipboardCopy } from 'src/components/ClipboardCopy';
-import { Page } from 'src/components/Page';
-import { createUseStyles } from 'src/lib/jss';
+import { AuthenticatedPage } from 'src/components/Page';
 import { useAuthenticatedUser } from 'src/services/Authentication';
-import { colors } from 'src/theme';
-import { getPlayer } from '../GameRoomV2/util';
-import { ChessBoard } from '../Games/Chess/components/ChessBoardV2';
-import { gameRecordToGame, pgnToFen } from '../Games/Chess/lib';
+import { gameRecordToGame } from '../Games/Chess/lib';
 import { Game } from '../Games/types';
 import { getUserGames } from './resources';
+import { GamesArchive } from './GamesArchive';
+
 
 type Props = {};
 
-const getGameResult = (game: Game, userId: UserRecord['id']) => {
-  if (game.winner === '1/2') {
-    return 'Draw';
-  }
-
-  const winningColor = game.winner;
-
-  const userPlayer = getPlayer(userId, game.players);
-
-  if (!userPlayer) {
-    return `${capitalize(winningColor || '')} Won`;
-  }
-
-  if (userPlayer.color === game.winner) {
-    return 'Won';
-  }
-
-  return 'Lost';
-}
-
-export const GamesArchivePage: React.FC<Props> = (props) => {
-  const cls = useStyles();
+export const GamesArchivePage: React.FC<Props> = () => {
   const user = useAuthenticatedUser();
   const [games, setGames] = useState<Game[]>([]);
 
-
   useEffect(() => {
-    if (!user) {
+    if (!(user && user.isGuest)) {
       return;
     }
 
@@ -50,51 +23,9 @@ export const GamesArchivePage: React.FC<Props> = (props) => {
     });
   }, [user]);
 
-  if (!user) {
-    return null;
-  }
-
   return (
-    <Page>
-      <div className={cls.container}>
-        {games.map((game) => (
-            <div className={cls.gameContainer} key={game.id}>
-              <div>
-                {game.createdAt}
-              </div>
-              <div>
-                {`${game.players[0].user.firstName} ${game.players[0].user.lastName}`}
-                {` - `}
-                {`${game.players[1].user.firstName} ${game.players[1].user.lastName}`}
-              </div>
-              <div className={cls.gameResult}>
-                {getGameResult(game, user.id)}
-              </div>
-              <div>
-                <div>
-                  <ClipboardCopy 
-                    value={game.pgn || ''}
-                  />
-                </div>
-              </div>
-              <div>
-                <ChessBoard fen={pgnToFen(game.pgn || '')} size={125} coordinates={false} />
-              </div>
-            </div>
-          ))}
-      </div>
-    </Page>
+    <AuthenticatedPage>
+      {user?.isGuest === false && <GamesArchive user={user} games={games} />}
+    </AuthenticatedPage>
   );
 };
-
-const useStyles = createUseStyles({
-  container: {},
-  gameContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    borderBottom: `1px solid ${colors.neutralLight}`,
-    padding: '16px',
-    justifyContent: 'space-between',
-  },
-  gameResult: {},
-});
