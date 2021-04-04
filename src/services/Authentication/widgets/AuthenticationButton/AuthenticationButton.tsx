cf8df827532d74f75fc00bd4a95cad0329ef18f6
Gabriel C. Troia <catalin.troia@gmail.com>
2021-04-03 23:58:30 -0600

    fix(authentiactionbutton): add "onClick" prop to be able to capture the event outside of it as well

--- src/services/Authentication/widgets/AuthenticationButton/AuthenticationButton.tsx
+++ src/services/Authentication/widgets/AuthenticationButton/AuthenticationButton.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { Button, ButtonProps } from 'src/components/Button';
+import { noop } from 'src/lib/util';
 import { AuthenticationDialog } from '../AuthenticationDialog';
 
 type Props = Omit<ButtonProps, 'onClick' | 'click' | 'type' | 'label'> & {
   label?: string;
+  onClick?: () => void;
 };
 
 export const AuthenticationButton: React.FC<Props> = ({
   label = 'My Account',
+  onClick = noop,
   ...restButtonProps
 }) => {
   const [showDialog, setShowDialog] = useState(false);
@@ -20,6 +23,8 @@ export const AuthenticationButton: React.FC<Props> = ({
         clear
         type="primary"
         onClick={() => {
+          onClick();
+
           setDialogUniqKey(String(Math.random()));
           setShowDialog(true);
         }}
