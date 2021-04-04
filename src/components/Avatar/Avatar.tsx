9d1bd9d5c8cadb5c9186ab6e66a3a82e136f9707
Gabriel C. Troia <catalin.troia@gmail.com>
2021-04-03 23:50:38 -0600

    refactor(avatar): add background color to the Mutunachi Avatars and refactor a bit

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,50 +1,70 @@
 import React from 'react';
 import { createUseStyles } from 'src/lib/jss';
-import cx from 'classnames';
-import { Avatar as GAvatar, AvatarProps as GAvatarProps } from 'grommet';
-import { colors, floatingShadow } from 'src/theme';
-import { onlyMobile } from 'src/theme';
+import { AvatarProps as GAvatarProps } from 'grommet';
+import { colors } from 'src/theme';
+import { AspectRatio } from '../AspectRatio';
+import { Mutunachi } from '../Mutunachi/Mutunachi';
 
 type Props = GAvatarProps & {
   className?: string;
   hasBorder?: boolean;
   darkMode?: boolean;
+  size?: string | number;
+
+  // TODO: Make it a union tag when we support profile pics
+  mutunachiId: number;
 };
+
+const bkgColors = [
+  colors.negativeLight,
+  colors.positiveLight,
+  colors.primaryLight,
+  colors.attentionLight,
+
+  // Better to have 5 or odd number
+  colors.attentionLight,
+];
+
+const getColor = (mid: number) => bkgColors[mid % bkgColors.length];
 
 export const Avatar: React.FC<Props> = ({
   className,
   hasBorder = false,
   darkMode = false,
+  size = 32,
+
   ...props
 }) => {
   const cls = useStyles();
 
   return (
-    <GAvatar
-      size="medium"
-      round="large"
-      className={cx(cls.container, className)}
-      {...props}
-      style={{
-        ...hasBorder && {
-          border: `2px solid ${darkMode ? colors.neutralLightest : colors.neutralDarkest}`,
-        },
-      }}
-    >
-      {props.children}
-    </GAvatar>
+    <div className={cls.container}>
+      <div
+        className={cls.mask}
+        style={{
+          width: size,
+          background: getColor(props.mutunachiId),
+          border: `1px solid ${darkMode ? colors.neutralLightest : colors.neutralLightest}`,
+        }}
+      >
+        <AspectRatio aspectRatio={1}>
+          <Mutunachi mid={props.mutunachiId} className={cls.mutunachiContainer} />
+        </AspectRatio>
+      </div>
+    </div>
   );
 };
 
 const useStyles = createUseStyles({
-  container: {
-    height: '32px !important',
-    width: '32px !important',
-    ...floatingShadow,
-
-    ...onlyMobile({
-      height: '24px !important',
-      width: '24px !important',
-    }),
+  container: {},
+  mask: {
+    overflow: 'hidden',
+    position: 'relative',
+    borderRadius: '50%',
+    zIndex: 1,
+  },
+  mutunachiContainer: {
+    width: '70%',
+    transform: 'translateX(20%) translateY(15%)',
   },
 });
