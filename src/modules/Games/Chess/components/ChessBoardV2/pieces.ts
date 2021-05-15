a5462d481092db980753e4f7dc0989da7b9ae4cf
Gabriel C. Troia <catalin.troia@gmail.com>
2021-05-15 17:57:40 -0500

    fix(pieces): fix an issue with the piece images not loading correctly

--- src/modules/Games/Chess/components/ChessBoardV2/pieces.ts
+++ src/modules/Games/Chess/components/ChessBoardV2/pieces.ts
@@ -1,15 +1,15 @@
-import whitePawn from 'node_modules/react-chessground/dist/images/pieces/merida/wP.svg';
-import whiteQueen from 'node_modules/react-chessground/dist/images/pieces/merida/wQ.svg';
-import whiteRook from 'node_modules/react-chessground/dist/images/pieces/merida/wR.svg';
-import whiteBishop from 'node_modules/react-chessground/dist/images/pieces/merida/wB.svg';
-import whiteKnight from 'node_modules/react-chessground/dist/images/pieces/merida/wN.svg';
-import whiteKing from 'node_modules/react-chessground/dist/images/pieces/merida/wK.svg';
-import blackPawn from 'node_modules/react-chessground/dist/images/pieces/merida/bP.svg';
-import blackQueen from 'node_modules/react-chessground/dist/images/pieces/merida/bQ.svg';
-import blackRook from 'node_modules/react-chessground/dist/images/pieces/merida/bR.svg';
-import blackBishop from 'node_modules/react-chessground/dist/images/pieces/merida/bB.svg';
-import blackKnight from 'node_modules/react-chessground/dist/images/pieces/merida/bN.svg';
-import blackKing from 'node_modules/react-chessground/dist/images/pieces/merida/bK.svg';
+import whitePawn from 'react-chessground/dist/images/pieces/merida/wP.svg';
+import whiteQueen from 'react-chessground/dist/images/pieces/merida/wQ.svg';
+import whiteRook from 'react-chessground/dist/images/pieces/merida/wR.svg';
+import whiteBishop from 'react-chessground/dist/images/pieces/merida/wB.svg';
+import whiteKnight from 'react-chessground/dist/images/pieces/merida/wN.svg';
+import whiteKing from 'react-chessground/dist/images/pieces/merida/wK.svg';
+import blackPawn from 'react-chessground/dist/images/pieces/merida/bP.svg';
+import blackQueen from 'react-chessground/dist/images/pieces/merida/bQ.svg';
+import blackRook from 'react-chessground/dist/images/pieces/merida/bR.svg';
+import blackBishop from 'react-chessground/dist/images/pieces/merida/bB.svg';
+import blackKnight from 'react-chessground/dist/images/pieces/merida/bN.svg';
+import blackKing from 'react-chessground/dist/images/pieces/merida/bK.svg';
 
 export const pieces = {
   whitePawn,
