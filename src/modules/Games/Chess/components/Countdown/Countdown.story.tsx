b8725a8e35e65a7feb880a200c200e30c96c8195
Gabriel C. Troia <catalin.troia@gmail.com>
2021-05-30 17:29:59 -0500

    fix(countdown): attemt to fix local weirdness by upgrading dateforma

--- src/modules/Games/Chess/components/Countdown/Countdown.story.tsx
+++ src/modules/Games/Chess/components/Countdown/Countdown.story.tsx
@@ -5,20 +5,38 @@ import { minutes, second, seconds } from 'src/lib/time';
 import { defaultTheme } from 'src/theme';
 import { Countdown } from './Countdown';
 
-
 export default {
   component: Countdown,
   title: 'modules/Games/Chess/Countdown',
 };
 
+export const defaultStory = () => (
+  <Grommet theme={defaultTheme}>
+    <div
+      style={{
+        paddingBottom: '1em',
+      }}
+    >
+      <Countdown timeLeft={seconds(30)} active gameTimeClass="bullet30" />
+    </div>
+    <div
+      style={{
+        paddingBottom: '1em',
+      }}
+    >
+      <Countdown timeLeft={seconds(56)} active gameTimeClass="blitz2" />
+    </div>
+  </Grommet>
+);
+
 export const bullet = () => (
   <Grommet theme={defaultTheme}>
-    <Countdown timeLeft={seconds(56)} active gameTimeClass="bullet30"/>
+    <Countdown timeLeft={seconds(56)} active gameTimeClass="bullet1" />
   </Grommet>
 );
 
 export const blitz = () => (
   <Grommet theme={defaultTheme}>
-    <Countdown timeLeft={minutes(1.03)} active gameTimeClass="blitz2"/>
+    <Countdown timeLeft={minutes(1.03)} active gameTimeClass="bullet1" />
   </Grommet>
 );
