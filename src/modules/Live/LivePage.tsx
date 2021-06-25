ce71690326e5f84f009684f73a0ffa997fb3c306
Gabriel C. Troia <catalin.troia@gmail.com>
2021-06-24 19:56:29 -0500

    feat(live): pull all the Twitch collaborators and show them on the Live Page

--- src/modules/Live/LivePage.tsx
+++ src/modules/Live/LivePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Page } from 'src/components/Page';
 import { createUseStyles, NestedCSSElement } from 'src/lib/jss';
 import { AspectRatio } from 'src/components/AspectRatio';
@@ -6,131 +6,122 @@ import ReactTwitchEmbedVideo from 'react-twitch-embed-video';
 import { spacers } from 'src/theme/spacers';
 import { effects } from 'src/theme';
 import { useDeviceSize } from 'src/theme/hooks/useDeviceSize';
+import { getCollaboratorsByPlatform } from './resources';
+import { CollaboratorRecord } from 'dstnd-io';
+
 
 type Props = {};
 
-const streamers = [
-  {
-    twitch: 'chesswithovi',
-    rank: 900,
-    on: false,
-  },
-  {
-    twitch: 'chess',
-    rank: 500,
-    on: true,
-  },
-  {
-    twitch: 'quaglon',
-    rank: 600,
-    on: false,
-  },
-  {
-    twitch: 'art_vega1983',
-    rank: 400,
-    on: false,
-  },
-  {
-    twitch: 'clsmith15',
-    rank: 510,
-    on: false,
-  },
-  {
-    twitch: 'thinkerteacher',
-    rank: 490,
-    on: true,
-  },
-  {
-    twitch: 'crybabycarly',
-    rank: 680,
-    on: false,
-  },
-  {
-    twitch: 'road2gm3000',
-    rank: 700,
-    on: false,
-  },
-];
-
-const getStreamers = (featuredTwitch?: string) => {
-  const [featured, ...orderedByRankDesk] = streamers
-    .map((s) => ({
-      ...s,
-      rank: s.rank + (s.on ? 1 : 0) * 1000 + (featuredTwitch === s.twitch ? 1 : 0) * 5000,
-    }))
-    .sort((a, b) => b.rank - a.rank);
-
-  return {
-    featured,
-    streamers: orderedByRankDesk,
-  };
+type StreamerCollection = {
+  featured: CollaboratorRecord;
+  restInRankedOrder: CollaboratorRecord[];
 };
+
+const processStreamers = ([
+  featured,
+  ...restInRankedOrder
+]: CollaboratorRecord[]): StreamerCollection => ({
+  featured,
+  restInRankedOrder,
+});
 
 export const LivePage: React.FC<Props> = (props) => {
   const cls = useStyles();
   const deviceSize = useDeviceSize();
-  const [streamersCollection, setStreamersCollection] = useState(getStreamers());
+  const [streamersCollection, setStreamersCollection] = useState<StreamerCollection>();
+
+  useEffect(() => {
+    getCollaboratorsByPlatform({
+      platform: 'Twitch',
+      pageSize: 10,
+      currentIndex: 0,
+    }).map((r) => {
+      setStreamersCollection(processStreamers(r.items));
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log('streamersCollection', streamersCollection);
+  }, [streamersCollection]);
 
   return (
     <Page name="Live">
-      <div className={cls.container}>
-        <div className={cls.main}>
-          <AspectRatio aspectRatio={{ width: 16, height: 9 }}>
-            {/* <div className={cls.videoContainer} /> */}
-            <ReactTwitchEmbedVideo
-              channel={streamersCollection.featured.twitch}
-              layout="video"
-              height="100%"
-              width="100%"
-              targetClass={cls.videoContainer}
-              targetId={streamersCollection.featured.twitch}
-            />
-          </AspectRatio>
-          <div
-            style={{
-              height: spacers.largestPx,
-            }}
-          />
-          <div className={cls.streamerCollectionListMask}>
-            <div className={cls.streamerCollectionList}>
-              {streamersCollection.streamers.map((s) => (
-                <AspectRatio
-                  aspectRatio={{ width: 16, height: 9 }}
-                  className={cls.aspect}
-                  onClick={() => setStreamersCollection(getStreamers(s.twitch))}
-                >
-                  <ReactTwitchEmbedVideo
-                    channel={s.twitch}
-                    layout="video"
-                    height="100%"
-                    width="100%"
-                    targetClass={cls.videoContainer}
-                    targetId={s.twitch}
-                    autoplay={false}
-                  />
-                </AspectRatio>
-              ))}
-            </div>
-          </div>
-        </div>
-        {deviceSize.isMobile || (
-          <>
+      {streamersCollection && (
+        <div className={cls.container}>
+          <div className={cls.main}>
+            <AspectRatio aspectRatio={{ width: 16, height: 9 }}>
+              {/* <div className={cls.videoContainer} /> */}
+              <ReactTwitchEmbedVideo
+                channel={streamersCollection.featured.profileUrl}
+                layout="video"
+                height="100%"
+                width="100%"
+                targetClass={cls.videoContainer}
+                targetId={streamersCollection.featured.profileUrl}
+              />
+            </AspectRatio>
             <div
               style={{
-                width: spacers.large,
+                height: spacers.large,
               }}
             />
-            <div className={cls.side}>
-              <iframe
-                src={`https://www.twitch.tv/embed/${streamersCollection.featured.twitch}/chat?parent=localhost`}
-                height="60% "
-                width="100%"
-                className={cls.chatContainer}
-              ></iframe>
+            <div className={cls.streamerCollectionListMask}>
+              <h3>Weekly Featured</h3>
+              {/* <Text size="subtitle1" asParagraph></Text> */}
+              {/* <br/>
+              <br/> */}
+              <div className={cls.streamerCollectionList}>
+                {streamersCollection.restInRankedOrder.map((s) => (
+                  <div
+                    className={cls.aspect}
+                    style={{
+                      overflow: 'hidden',
+                      backgroundColor: '#ededed',
+                      borderRadius: '16px',
+                    }}
+                  >
+                    <AspectRatio
+                      aspectRatio={{ width: 4, height: 3 }}
+                      // className={cls.aspect}
+                      // onClick={() => setStreamersCollection(getStreamers(s.twitch))}
+                    >
+                      {/* <div style={{}}> */}
+                      <img src={s.profilePicUrl} width="100%" />
+                      {/* </div> */}
+                      {/* <ReactTwitchEmbedVideo
+                      channel={s.profileUrl}
+                      layout="video"
+                      height="100%"
+                      width="100%"
+                      targetClass={cls.videoContainer}
+                      targetId={s.profilePicUrl}
+                      autoplay={false}
+                    /> */}
+                    </AspectRatio>
+                  </div>
+                ))}
+              </div>
             </div>
-          </>
-        )}
-      </div>
+          </div>
+          {deviceSize.isMobile || (
+            <>
+              <div
+                style={{
+                  width: spacers.large,
+                }}
+              />
+              <div className={cls.side}>
+                <iframe
+                  src={`https://www.twitch.tv/embed/${streamersCollection.featured.profileUrl}/chat?parent=localhost`}
+                  height="65%"
+                  width="100%"
+                  className={cls.chatContainer}
+                ></iframe>
+              </div>
+            </>
+          )}
+        </div>
+      )}
     </Page>
   );
 };
@@ -148,9 +139,9 @@ const useStyles = createUseStyles({
   },
   videoContainer: {
     border: 0,
+    backgroundColor: '#ededed',
     borderRadius: '16px',
     overflow: 'hidden',
-    backgroundColor: '#ededed',
 
     width: '100%',
     height: '100%',
