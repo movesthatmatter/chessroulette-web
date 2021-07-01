14606660eb477dbfa00aa46197fa633d39ce0d1a
Gabriel C. Troia <catalin.troia@gmail.com>
2021-07-01 12:10:06 -0500

    style(authenticationdialog): fix the padding of the Twitch Button

--- src/services/Authentication/components/VerificationForm/VerificationForm.tsx
+++ src/services/Authentication/components/VerificationForm/VerificationForm.tsx
@@ -16,11 +16,11 @@ import { Emoji } from 'src/components/Emoji';
 import { TwitchAuthButton } from 'src/vendors/twitch/TwitchAuthButton/TwitchAuthButton';
 import { spacers } from 'src/theme/spacers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faTwitch} from '@fortawesome/free-brands-svg-icons';
+import { faTwitch } from '@fortawesome/free-brands-svg-icons';
 
 type Model = {
   code: string;
-}
+};
 
 type Props = {
   onSubmit: (r: UserAccountInfo) => AsyncResult<void, SubmissionErrors<Model>>;
@@ -33,76 +33,78 @@ export const VerificationForm: React.FC<Props> = (props) => {
 
   return (
     <div>
-      {emailToBeVerified
-        ? (
-          <CodeVerificationForm
-            emailToBeVerified={emailToBeVerified}
-            onSubmit={props.onSubmit}
-            infoText={(
-              <>
-                I just sent you an email at <strong>{emailToBeVerified}</strong>
-                {` `} with a code!<br /> You know what to do <Emoji symbol="😎" />
-              </>
-            )}
-          />
-        ) : (
-          <Form<{
-            email: string;
-          }>
-            key="email-form"
-            onSubmit={(model) => {
-              return resources
-                .verifyEmail(model)
-                .map(() => setEmailToBeVerified(model.email))
-                .mapErr((e) => {
-                  if (e.type === 'ValidationErrors') {
-                    return {
-                      type: 'SubmissionValidationErrors',
-                      content: e.content,
-                    };
-                  }
+      {emailToBeVerified ? (
+        <CodeVerificationForm
+          emailToBeVerified={emailToBeVerified}
+          onSubmit={props.onSubmit}
+          infoText={
+            <>
+              I just sent you an email at <strong>{emailToBeVerified}</strong>
+              {` `} with a code!
+              <br /> You know what to do <Emoji symbol="😎" />
+            </>
+          }
+        />
+      ) : (
+        <Form<{
+          email: string;
+        }>
+          key="email-form"
+          onSubmit={(model) => {
+            return resources
+              .verifyEmail(model)
+              .map(() => setEmailToBeVerified(model.email))
+              .mapErr((e) => {
+                if (e.type === 'ValidationErrors') {
+                  return {
+                    type: 'SubmissionValidationErrors',
+                    content: e.content,
+                  };
+                }
 
-                  if (e.type === 'EmailCantSendError') {
-                    return {
-                      type: 'SubmissionGenericError',
-                      content: `Ooops – This one's on me! I couldn't send you email now but please try again in a few moments.`,
-                    };
-                  }
-
+                if (e.type === 'EmailCantSendError') {
                   return {
                     type: 'SubmissionGenericError',
-                    content: e.content,
-                  }
-                });
-            }}
-            validator={{
-              email: [validator.rules.email(), validator.messages.email],
-            }}
-            render={(p) => (
-              <>
-                <TextInput
-                  key="email"
-                  label="What's your Email?"
-                  placeholder="beth.harmon@queens.gambit"
-                  onChange={(e) => p.onChange('email', e.target.value)}
-                  onBlur={() => p.validateField('email')}
-                  validationError={
-                    p.errors.validationErrors?.email || p.errors.submissionValidationErrors?.email
-                  }
-                />
-                {p.errors.submissionGenericError && <FormError message={p.errors.submissionGenericError} />}
-                <div style={{ paddingBottom: '6px' }} />
-                <Button
-                  label="Send Verification"
-                  full
-                  type="positive"
-                  withLoader
-                  onClick={p.submit}
-                />
-              </>
-            )}
-          />
-        )}
+                    content: `Ooops – This one's on me! I couldn't send you email now but please try again in a few moments.`,
+                  };
+                }
+
+                return {
+                  type: 'SubmissionGenericError',
+                  content: e.content,
+                };
+              });
+          }}
+          validator={{
+            email: [validator.rules.email(), validator.messages.email],
+          }}
+          render={(p) => (
+            <>
+              <TextInput
+                key="email"
+                label="What's your Email?"
+                placeholder="beth.harmon@queens.gambit"
+                onChange={(e) => p.onChange('email', e.target.value)}
+                onBlur={() => p.validateField('email')}
+                validationError={
+                  p.errors.validationErrors?.email || p.errors.submissionValidationErrors?.email
+                }
+              />
+              {p.errors.submissionGenericError && (
+                <FormError message={p.errors.submissionGenericError} />
+              )}
+              <div style={{ paddingBottom: '6px' }} />
+              <Button
+                label="Send Verification"
+                full
+                type="positive"
+                withLoader
+                onClick={p.submit}
+              />
+            </>
+          )}
+        />
+      )}
       <Hr text="Or Continue With" />
       <div className={cls.buttonRows}>
         <LichessAuthButton
@@ -117,25 +119,25 @@ export const VerificationForm: React.FC<Props> = (props) => {
             });
           }}
         />
-        <div style={{height: spacers.smallPx}}/>
         <TwitchAuthButton
-        full
-        label='Twitch'
-        type='primary'
-        onSuccess={(accessToken) =>{
-          props.onSubmit({
-            type: 'external',
-            vendor: 'twitch',
-            accessToken
-          })
-        }}
-        style={{backgroundColor: '#6441a5'}}
-        icon={() => <FontAwesomeIcon icon={faTwitch} color='white' size='lg'/>}
-        iconWrapperStyle={{
-          backgroundColor: 'rgba(255, 255, 255, 0)',
-          padding: '0px'
-        }}
+          full
+          label="Twitch"
+          type="primary"
+          onSuccess={(accessToken) => {
+            props.onSubmit({
+              type: 'external',
+              vendor: 'twitch',
+              accessToken,
+            });
+          }}
+          style={{ backgroundColor: '#6441a5' }}
+          icon={() => <FontAwesomeIcon icon={faTwitch} color="white" size="lg" />}
+          iconWrapperStyle={{
+            backgroundColor: 'rgba(255, 255, 255, 0)',
+            padding: '0px',
+          }}
         />
+        <div style={{ height: spacers.smallPx }} />
         {/* <div style={{ width: '16px' }} />
         <FacebookAuthButton
           label="Facebook"
