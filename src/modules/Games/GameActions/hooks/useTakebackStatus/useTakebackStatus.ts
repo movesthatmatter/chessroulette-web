54f26df95f74ba2922278eaf94060142edebbda2
Andrei Topli <andrei.topli@gmail.com>
2021-07-19 02:11:56 -0500

    Clear console logs from useTakebackStatus hook

--- src/modules/Games/GameActions/hooks/useTakebackStatus/useTakebackStatus.ts
+++ src/modules/Games/GameActions/hooks/useTakebackStatus/useTakebackStatus.ts
@@ -36,30 +36,13 @@ export const useTakebackStatus = (
       if (game.history.length === 0){
         return false;
       }
-      // if (logArrayOrdered.length === 0){
-      //   console.log('RETURN ---- ARRAY IS 0!!!!!')
-      //   return true;
-      // }
       const lastNotification = logArrayOrdered[logArrayOrdered.length-1];
-      // console.log('LAST NOTIFICATION ', lastNotification);
-      // console.log('LOOOOOG=> ', logArrayOrdered);
       if (gameAndOfferZipWithPrev.prev.offer?.type === 'takeback'){
         if (isOfferNotification(lastNotification) && (lastNotification.status === 'withdrawn')){
-          console.log('RETURN ---- LAST WITHDRAWN TAKEBACK!!!!!')
           return true;
         };
-        // if (gameAndOfferZipWithPrev.prev.game.pgn !== gameAndOfferZipWithPrev.current.game.pgn){
-        //   console.log('RETURN ---- OFFER WITH PREV PGN CHANGED !!!')
-        //   return true
-        // };
-        console.log('RETURN ---- OFFER WITTH PREV IS TAKEBACK!!!!')
         return false;
       }
-    // if (game.lastMoveBy !== meAsPlayer.color && gameAndOfferZipWithPrev.prev.offer?.type !== 'takeback') {
-    //     console.log('RETURN ---- LAST MOVOE!!!!!!!')
-    //     return false;
-    // }
-      console.log('RETURN ---- NO CONDITION!!!!')
       return true;
     })
   },[gameAndOfferZipWithPrev, logArrayOrdered, game])
