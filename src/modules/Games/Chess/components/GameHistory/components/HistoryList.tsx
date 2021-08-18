5d0e12b5e369cd61807ee67b366003b7a814a23f
Gabriel C. Troia <catalin.troia@gmail.com>
2021-08-18 18:58:36 -0500

    feat(history list): add ability to scroll on focus to the focused row

--- src/modules/Games/Chess/components/GameHistory/components/HistoryList.tsx
+++ src/modules/Games/Chess/components/GameHistory/components/HistoryList.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import { debounce } from 'debounce';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   ChessAnalysisHistory,
   ChessHistoryIndex,
+  getChessHistoryMoveIndex,
 } from 'src/modules/Room/RoomActivity/activities/AnalysisActivity/lib';
 import {
   isPartialBlackMove,
@@ -19,6 +21,10 @@ export type HistoryListProps = {
   rootPairedIndex?: number;
 };
 
+const scrollIntoView = debounce((elm: HTMLDivElement) => {
+  elm.scrollIntoView({ block: 'end', behavior: 'smooth' });
+}, 5);
+
 export const HistoryList: React.FC<HistoryListProps> = ({
   history,
   focusedIndex,
@@ -26,29 +32,56 @@ export const HistoryList: React.FC<HistoryListProps> = ({
   className,
   rootPairedIndex = 0,
 }) => {
-  const [pairedHistory, setPairedHistory] = useState<PairedHistory>([]);
+  const rowElementRefs = useRef<Record<number, HTMLDivElement | null>>({});
+  const containerElementRef = useRef<HTMLDivElement | null>();
+  const [pairedHistory, setPairedHistory] = useState<PairedHistory>(toPairedHistory(history));
 
   useEffect(() => {
     setPairedHistory(toPairedHistory(history));
   }, [history]);
 
+  useEffect(() => {
+    if (pairedHistory.length === 0) {
+      return;
+    }
+
+    const moveIndex = Math.floor(getChessHistoryMoveIndex(focusedIndex) / 2);
+
+    const elm = rowElementRefs.current[moveIndex];
+    if (elm) {
+      scrollIntoView(elm);
+    }
+  }, [pairedHistory, focusedIndex]);
+
+  useEffect(() => {
+    if (containerElementRef.current) {
+      containerElementRef.current.scrollTo(0, 9999);
+    }
+  }, []);
+
   return (
-    <div className={className}>
+    <div className={className} ref={(e) => (containerElementRef.current = e)}>
       {pairedHistory.map((pairedMove, index) => (
-        <HistoryRow
+        <div
           key={`${pairedMove[0].san}-${pairedMove[1]?.san || ''}`}
-          pairedMove={pairedMove}
-          pairedIndex={rootPairedIndex + index}
-          startingLinearIndex={
-            isPartialBlackMove(pairedHistory[0])
-              ? // If the history starts with a black move the index needs to be altered
-                //  but have no idea how come it needs to be substracted not added
-                pairedToLinearIndex([index, 0]) - 1
-              : pairedToLinearIndex([index, 0])
-          }
-          focusedIndex={focusedIndex}
-          onFocus={onRefocus}
-        />
+          ref={(b) => (rowElementRefs.current[index] = b)}
+        >
+          {/* <>{index} - {pairedToLinearIndex([index, 0])} </> */}
+          <HistoryRow
+            key={`${pairedMove[0].san}-${pairedMove[1]?.san || ''}`}
+            pairedMove={pairedMove}
+            pairedIndex={rootPairedIndex + index}
+            startingLinearIndex={
+              isPartialBlackMove(pairedHistory[0])
+                ? // If the history starts with a black move the index needs to be altered
+                  //  but have no idea how come it needs to be substracted not added
+                  pairedToLinearIndex([index, 0]) - 1
+                : pairedToLinearIndex([index, 0])
+            }
+            focusedIndex={focusedIndex}
+            onFocus={onRefocus}
+          />
+        </div>
       ))}
     </div>
   );
