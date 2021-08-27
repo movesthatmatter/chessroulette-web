e5aeb42664777fa2eea6dc8e9b002874d9506bcc
Gabriel C. Troia <catalin.troia@gmail.com>
2021-08-27 15:05:50 -0500

    feat(floatingbox): add a new component to abstract away the floating/round borders white bkg

---
+++ src/components/FloatingBox/FloatingBox.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createUseStyles } from 'src/lib/jss';
+import { colors, floatingShadow, softBorderRadius } from 'src/theme';
+import { spacers } from 'src/theme/spacers';
+import cx from 'classnames';
+
+export type FloatingBoxProps = React.HTMLProps<HTMLDivElement> & {};
+
+export const FloatingBox: React.FC<FloatingBoxProps> = ({ children, className, ...divProps }) => {
+  const cls = useStyles();
+
+  return (
+    <div className={cx(cls.container, className)} {...divProps}>
+      {children}
+    </div>
+  );
+};
+
+const useStyles = createUseStyles({
+  container: {
+    ...floatingShadow,
+    ...softBorderRadius,
+    padding: spacers.default,
+    background: colors.white,
+  },
+});
