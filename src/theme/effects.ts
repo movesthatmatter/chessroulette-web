9c568848bc7cd62c9a8f279642b5648219ddac66
Gabriel C. Troia <catalin.troia@gmail.com>
2021-08-27 19:22:54 -0500

    style(floatingshadow): increase the visibility

--- src/theme/effects.ts
+++ src/theme/effects.ts
@@ -1,7 +1,7 @@
 import { CSSProperties } from 'src/lib/jss/types';
 
 export const floatingShadow: CSSProperties = {
-  boxShadow: '0 12px 26px rgba(16, 30, 115, 0.06)',
+  boxShadow: '0 12px 26px rgba(16, 30, 115, 0.08)',
 };
 
 export const softBorderRadius: CSSProperties = {
