a5df18adcc398fe614ef94f5bacb1880bd1d83dd
Gabriel C. Troia <catalin.troia@gmail.com>
2021-09-14 10:37:00 -0500

    feat(usesession): make this more like a store, where you don't have to interact w redux
    This is better because the highlevel components could just use the hook for saving and that's it

---
+++ src/services/Session/type.ts
@@ -0,0 +1,1 @@
+export type SessionState = Record<string, unknown>;
--- src/services/Session/useSession.ts
+++ src/services/Session/useSession.ts
@@ -1,4 +1,40 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectSession } from './selector';
+import { setSessionAction } from './reducer';
+import { useEffect } from 'react';
 
-export const useSession = () => useSelector(selectSession);
+type TValue = unknown;
+type TValueSetter<T extends TValue> = (valueGetter: T | ((prev: T) => T)) => void;
+
+export function useSession<T extends TValue>(
+  key: string
+): [T | undefined, TValueSetter<T | undefined>];
+export function useSession<T extends TValue>(key: string, defaultValue: T): [T, TValueSetter<T>];
+export function useSession<T>(key: string, defaultValue?: T) {
+  const dispatch = useDispatch();
+  const existentValue = useSelector(selectSession)[key] as T;
+
+  // This only needs to happen on mount
+  useEffect(() => {
+    if (!existentValue) {
+      dispatch(setSessionAction({ key, value: defaultValue }));
+    }
+  }, []);
+
+  const currentValue = typeof existentValue !== 'undefined' ? existentValue : defaultValue;
+
+  return [
+    currentValue,
+    (valueGetter: T | ((prev: T) => T)) => {
+      const value =
+        typeof valueGetter === 'function' ? (valueGetter as any)(currentValue) : valueGetter;
+
+      dispatch(
+        setSessionAction({
+          key,
+          value,
+        })
+      );
+    },
+  ];
+}
