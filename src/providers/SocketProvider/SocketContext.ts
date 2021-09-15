44b30246ccc25ca602d21634b28c27fad0af1a1f
Gabriel C. Troia <catalin.troia@gmail.com>
2021-09-15 09:51:40 -0500

    feat(socketprovider): use a status for the Socket Provider. Remove fallbackrender from Consumer

--- src/providers/SocketProvider/SocketContext.ts
+++ src/providers/SocketProvider/SocketContext.ts
@@ -1,14 +1,24 @@
-import { createContext } from "react";
-import { SocketClient } from "src/services/socket/SocketClient";
+import { createContext } from 'react';
+import { SocketClient } from 'src/services/socket/SocketClient';
+import { SocketConnectionStatusNonOpen, SocketConnectionStatusOpen } from './types';
 
 export type SocketContextProps = {
-  socket: SocketClient | undefined;
   consumers: { [consumerId: string]: null };
   onDemand: () => () => void;
-};
+} & (
+  | {
+      socket: SocketClient;
+      status: SocketConnectionStatusOpen;
+    }
+  | {
+      socket: undefined;
+      status: SocketConnectionStatusNonOpen;
+    }
+);
 
 export const SocketContext = createContext<SocketContextProps>({
   socket: undefined,
+  status: 'init',
   consumers: {},
   onDemand: () => () => undefined,
 });
--- src/providers/SocketProvider/types.ts
+++ src/providers/SocketProvider/types.ts
@@ -1,3 +1,20 @@
 import { SocketClient } from 'src/services/socket/SocketClient';
 
 export type SocketReceivableMessage = Parameters<Parameters<SocketClient['onMessage']>[0]>[0];
+
+// Taken from here
+// https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/readyState#value
+export type SocketConnectionStatus =
+  | 'connecting'
+  | 'open'
+  | 'closing'
+  | 'closed'
+  // These are new
+  | 'init' // Initial State
+  | 'disconnected'; // it was open, now it's closed
+
+export type SocketConnectionStatusOpen = Extract<SocketConnectionStatus, 'open'>;
+export type SocketConnectionStatusNonOpen = Exclude<
+  SocketConnectionStatus,
+  SocketConnectionStatusOpen
+>;
