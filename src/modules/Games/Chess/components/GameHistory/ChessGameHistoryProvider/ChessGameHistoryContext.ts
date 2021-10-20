8561ed4ae573493fd8e89cb56ea9184ef60ffa73
Gabriel C. Troia <catalin.troia@gmail.com>
2021-10-20 18:22:36 -0500

    refactor(chessgamehistoryprovider): add a displayed obj on the context that holds an atomic snapshot
    This currently encapsulated::
    - history
    - pgn
    - index
    - fen
    
    Adnt it's guaranteed that they are all in sync, meaning if one changes it's ok to check the fen or pgn
    for updates.
    
    In additoin the ChessGame was changed to use the new api, and not call the useGameActions.onMove by itself
    anymore, but to only call a passed prop that happens to come from the GameHistoryConsumer.onAddMove.
    
    This way the GameHistoryProvider can setup the display props before the server returns a result, which
    ensures the client is fast.

--- src/modules/Games/Chess/components/GameHistory/ChessGameHistoryProvider/ChessGameHistoryContext.ts
+++ src/modules/Games/Chess/components/GameHistory/ChessGameHistoryProvider/ChessGameHistoryContext.ts
@@ -1,20 +1,47 @@
-import { ChessHistory, ChessHistoryIndex, ChessHistoryMove } from 'dstnd-io';
+import { FEN } from 'chessground/types';
+import {
+  ChessGameStateFen,
+  ChessHistory,
+  ChessHistoryIndex,
+  ChessHistoryMove,
+  SimplePGN,
+} from 'dstnd-io';
 import { createContext } from 'react';
+import { noop } from 'src/lib/util';
 
 export type ChessGameHistoryContextProps = {
   history: ChessHistory;
-  displayedIndex: ChessHistoryIndex;
-  displayedHistory: ChessHistory;
+  // This is used only for checking if the states should upadte!
+  cachedHistoryFen: FEN;
+
+  displayed: {
+    index: ChessHistoryIndex;
+    history: ChessHistory;
+    pgn: SimplePGN;
+    // The displayedFen acts as a chcecksum of the displayedHistory & displayedIndex
+    //  so we don't have to compare both anymore!
+    fen: ChessGameStateFen;
+  };
+
   onReset: () => void;
   onRefocus: (nextIndex: ChessHistoryIndex) => void;
-  onAddMove: (move: ChessHistoryMove, atIndex: ChessHistoryIndex, withRefocus?: boolean) => void;
+  onAddMove: (p: {
+    move: ChessHistoryMove;
+    atIndex?: ChessHistoryIndex;
+    withRefocus?: boolean;
+  }) => void;
 };
 
 export const ChessGameHistoryContext = createContext<ChessGameHistoryContextProps>({
   history: [],
-  displayedIndex: 0,
-  displayedHistory: [],
-  onReset: () => {},
-  onRefocus: () => {},
-  onAddMove: () => {},
+  cachedHistoryFen: '',
+  displayed: {
+    index: 0,
+    history: [],
+    pgn: '' as SimplePGN,
+    fen: '',
+  },
+  onReset: noop,
+  onRefocus: noop,
+  onAddMove: noop,
 });
--- src/modules/Games/Chess/components/GameHistory/ChessGameHistoryProvider/ChessGameHistoryProvider.tsx
+++ src/modules/Games/Chess/components/GameHistory/ChessGameHistoryProvider/ChessGameHistoryProvider.tsx
@@ -1,9 +1,12 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import useEventListener from '@use-it/event-listener';
 import { ChessHistory, ChessHistoryIndex, ChessHistoryMove, analysis } from 'dstnd-io';
 import { keyInObject, noop } from 'src/lib/util';
 import { ChessGameHistoryContext, ChessGameHistoryContextProps } from './ChessGameHistoryContext';
 import { useDebouncedCallback } from 'use-debounce';
+import { useStateWithCallback } from 'src/lib/hooks/useStateWithCallback';
+import { historyToFen, pgnToFen } from '../../../lib';
+import { chessHistoryToSimplePgn } from 'dstnd-io/dist/chessGame/util/util';
 
 type Props = {
   history: ChessHistory;
@@ -27,18 +30,31 @@ export const ChessGameHistoryProvider: React.FC<Props> = ({
     setContextState((prev) => ({
       ...prev,
       history,
-      displayedHistory: history,
-      displayedIndex: displayedIndex === undefined ? history.length - 1 : displayedIndex,
+      displayed: {
+        history,
+        index: displayedIndex === undefined ? history.length - 1 : displayedIndex,
+        fen: historyToFen(history),
+        pgn: chessHistoryToSimplePgn(history),
+      },
     }));
   }, [history]);
 
   const onRefocus = useCallback(
     (nextIndex: ChessHistoryIndex) => {
-      setContextState((prev) => ({
-        ...prev,
-        displayedIndex: nextIndex,
-        displayedHistory: analysis.actions.getChessHistoryAtIndex(prev.history, nextIndex),
-      }));
+      setContextState((prev) => {
+        const nextHistory = analysis.actions.getChessHistoryAtIndex(prev.history, nextIndex);
+        const nextPgn = chessHistoryToSimplePgn(nextHistory);
+
+        return {
+          ...prev,
+          displayed: {
+            index: nextIndex,
+            history: nextHistory,
+            pgn: nextPgn,
+            fen: pgnToFen(nextPgn),
+          },
+        };
+      });
 
       // TODO: This isn't the best here for now, since it doesn't actually check
       // that the refocus actually worked, meaning it got updated and it was not
@@ -55,49 +71,114 @@ export const ChessGameHistoryProvider: React.FC<Props> = ({
   });
 
   const onAddMove = useCallback(
-    (move: ChessHistoryMove, atIndex: ChessHistoryIndex, withRefocus = true) => {
-      setContextState((prev) => {
-        const [
-          nextHistory,
-          addedAtIndex,
-        ] = analysis.actions.addMoveToChessHistoryAtNextAvailableIndex(prev.history, atIndex, move);
+    // Move here should be a regular ChessMove (the action should transform it into ChessHistoryMove)!
+    ({
+      move,
+      atIndex, // If not given it will be appended at the end of the main history
+      withRefocus = true,
+    }: {
+      move: ChessHistoryMove;
+      atIndex?: ChessHistoryIndex;
+      withRefocus?: boolean;
+    }) => {
+      setContextState(
+        (prev) => {
+          const addAtIndex = atIndex || prev.history.length;
+          const [
+            nextHistory,
+            addedAtIndex,
+            // This shouldn't be branded as analysis as it's used for the game now as well, but something
+            //  more along the lines of ChessHistory which happen to be used by analysis (and game)!
+          ] = analysis.actions.addMoveToChessHistoryAtNextAvailableIndex(
+            prev.history,
+            addAtIndex,
+            move
+          );
+          const nextCachedHistoryFen = historyToFen(nextHistory);
+          const nextDisplayedHistory = analysis.actions.getChessHistoryAtIndex(
+            nextHistory,
+            addedAtIndex
+          );
+          const nextDisplayedPgn = chessHistoryToSimplePgn(nextDisplayedHistory);
 
-        return {
-          ...prev,
-          history: nextHistory,
-          ...(withRefocus && {
-            displayedIndex: addedAtIndex,
-            displayedHistory: analysis.actions.getChessHistoryAtIndex(nextHistory, addedAtIndex),
-          }),
-        };
-      });
-
-      // TODO: This isn't the best here for now, since it doesn't actually check
-      // that the refocus actually worked, meaning it got updated and it was not
-      onMoved(move, atIndex);
+          return {
+            ...prev,
+            history: nextHistory,
+            cachedHistoryFen: nextCachedHistoryFen,
+            ...(withRefocus && {
+              // All of these could actually come from the addMove library
+              //   (and eventually even from the server since all the computations are done anyway)
+              displayed: {
+                index: addedAtIndex,
+                history: nextDisplayedHistory,
+                pgn: nextDisplayedPgn,
+                fen: pgnToFen(nextDisplayedPgn),
+              },
+            }),
+          };
+        },
+        (nextState) => {
+          onMoved(move, nextState.displayed.index);
+        }
+      );
     },
     [history]
   );
 
-  const [contextState, setContextState] = useState<ChessGameHistoryContextProps>({
-    history,
-    displayedIndex: history.length - 1,
-    displayedHistory: history,
-    onReset,
-    onRefocus,
-    onAddMove,
-  });
+  const [contextState, setContextState] = useStateWithCallback<ChessGameHistoryContextProps>(
+    (() => {
+      const historyPgn = chessHistoryToSimplePgn(history);
+      const cachedHistoryFen = pgnToFen(historyPgn);
+
+      return {
+        history,
+        cachedHistoryFen,
+        // All of the displayedProps including the cachedFen
+        /// could be moved on the Server in the addMove function
+        displayed: {
+          index: history.length - 1,
+          history,
+          fen: cachedHistoryFen,
+          pgn: historyPgn,
+        },
+
+        onReset,
+        onRefocus,
+        onAddMove,
+      };
+    })()
+  );
 
   useEffect(() => {
     setContextState((prev) => {
       const nextIndex = displayedIndex === undefined ? history.length - 1 : displayedIndex;
       const nextDisplayedHistory = analysis.actions.getChessHistoryAtIndex(history, nextIndex);
 
+      // Use this to check wether this needs to update or not
+      // TODO: These should actually come from the server using the same library as on the
+      //   client to guarantee a match ((chessHistory|analysis).addMove as o)
+      const nextCachedHistoryFen = historyToFen(history);
+      const nextDisplayedPgn = chessHistoryToSimplePgn(nextDisplayedHistory);
+      const nextDisplayedFen = pgnToFen(nextDisplayedPgn);
+
+      // Return early if the cachedFen or displayedFen haven't changed
+      //   Note: the displayedFen takes in acctount the displyedIndex change!
+      if (
+        prev.displayed.fen === nextDisplayedFen &&
+        prev.cachedHistoryFen === nextCachedHistoryFen
+      ) {
+        return prev;
+      }
+
       return {
         ...prev,
         history,
-        displayedHistory: nextDisplayedHistory,
-        displayedIndex: nextIndex,
+        displayed: {
+          history: nextDisplayedHistory,
+          index: nextIndex,
+          pgn: nextDisplayedPgn,
+          fen: nextDisplayedFen,
+        },
       };
     });
   }, [history, displayedIndex]);
@@ -108,21 +189,21 @@ export const ChessGameHistoryProvider: React.FC<Props> = ({
     }
 
     const normalizedDisplayedIndex = analysis.actions.normalizeChessHistoryIndex(
-      contextState.displayedIndex
+      contextState.displayed.index
     );
 
     const lastInCurrentBranch = analysis.actions.getBranchedHistoryLastIndex(
       contextState.history,
-      contextState.displayedIndex
+      contextState.displayed.index
     );
     const normalizedLastInCurrentBranch = analysis.actions.normalizeChessHistoryIndex(
       lastInCurrentBranch
     );
 
     if (event.key === 'ArrowRight' && normalizedDisplayedIndex < normalizedLastInCurrentBranch) {
-      onRefocus(analysis.actions.incrementChessHistoryIndex(contextState.displayedIndex));
+      onRefocus(analysis.actions.incrementChessHistoryIndex(contextState.displayed.index));
     } else if (event.key === 'ArrowLeft' && normalizedDisplayedIndex >= 0) {
-      onRefocus(analysis.actions.decrementChessHistoryIndex(contextState.displayedIndex));
+      onRefocus(analysis.actions.decrementChessHistoryIndex(contextState.displayed.index));
     }
   });
 
