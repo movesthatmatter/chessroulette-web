fa8ead8cb572aee182369e0556ec43aff4709a14
Gabriel C. Troia <catalin.troia@gmail.com>
2022-03-09 16:13:18 -0600

    feat(facetime): add loader state to Facetime

--- src/components/FaceTime/FaceTime/FaceTime.tsx
+++ src/components/FaceTime/FaceTime/FaceTime.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import cx from 'classnames';
 import { createUseStyles } from 'src/lib/jss';
 import { VideoBox, VideoBoxProps } from 'src/components/VideoBox';
@@ -7,10 +7,13 @@ import { Text } from 'src/components/Text';
 import { AspectRatio, AspectRatioProps } from 'src/components/AspectRatio';
 import { colors } from 'src/theme/colors';
 import { PeerStreamingConfig } from 'src/providers/PeerProvider';
+import Loader from 'react-loaders';
+import 'loaders.css';
 
 export type FaceTimeProps = Omit<VideoBoxProps, 'stream'> & {
   streamConfig: PeerStreamingConfig;
-  streamingOffFallback?: React.ReactNode;
+  streamingOffFallback?: ReactNode;
+  loadingFallback?: ReactNode;
 
   aspectRatio?: AspectRatioProps['aspectRatio'];
   containerClassName?: string;
@@ -38,22 +41,40 @@ export const FaceTime: React.FC<FaceTimeProps> = ({
     width: 4,
     height: 3,
   },
+  loadingFallback = null,
   ...avStreamProps
 }) => {
   const cls = useStyles();
+  const [loadingVideo, setLoadingVideo] = useState(true);
+
+  const loader = (
+    <div className={cls.loadingWrapper}>
+      {loadingFallback || (
+        <div className={cls.loader}>
+          <Loader type="line-scale-pulse-out" active innerClassName={cls.loader} />
+        </div>
+      )}
+    </div>
+  );
 
   return (
     <div className={cx(cls.container, containerClassName)}>
       <AspectRatio aspectRatio={aspectRatio}>
         {streamConfig.on ? (
-          <VideoBox
-            stream={streamConfig.stream}
-            autoPlay
-            className={cx(cls.video, className)}
-            {...avStreamProps}
-          />
+          <>
+            <VideoBox
+              stream={streamConfig.stream}
+              autoPlay
+              className={cx(cls.video, className)}
+              {...avStreamProps}
+              onCanPlay={() => {
+                setLoadingVideo(false);
+              }}
+            />
+            {loadingVideo && loader}
+          </>
         ) : (
-          streamingOffFallback
+          streamingOffFallback || loader
         )}
         <div className={cls.overlayedContainer}>
           {headerOverlay && <div className={cls.headerWrapper}>{headerOverlay}</div>}
@@ -126,5 +147,20 @@ const useStyles = createUseStyles<CustomTheme>((theme) => ({
       paddingLeft: '8px',
       paddingBottom: '2px',
     }),
+  },
+  loadingWrapper: {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    position: 'absolute',
+    zIndex: 99,
+    display: 'flex',
+  },
+  loader: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 }));
