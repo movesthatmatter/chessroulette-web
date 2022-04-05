dcb5c2ab0194f3b75914e44188ade377cee75ca2
Gabriel C. Troia <catalin.troia@gmail.com>
2022-04-05 01:34:40 -0500

    feat(tournament): make the rounds be inferred from the matches

--- src/modules/Tournaments/TournamentPage/components/Bracket/Bracket.tsx
+++ src/modules/Tournaments/TournamentPage/components/Bracket/Bracket.tsx
@@ -1,65 +1,78 @@
+import React, { useEffect, useMemo, useState } from 'react';
 import { TournamentWithFullDetailsRecord } from 'chessroulette-io/dist/resourceCollections/tournaments/records';
-import React, { useEffect, useState } from 'react';
 import { Text } from 'src/components/Text';
 import { createUseStyles } from 'src/lib/jss';
+import { range } from 'src/lib/util';
 import { MatchViewer } from 'src/modules/Tournaments/components/MatchViewer/MatchViewer';
 import { indexMatchesByRound } from 'src/modules/Tournaments/utils';
 import { spacers } from 'src/theme/spacers';
 
 type Props = {
-	tournament: TournamentWithFullDetailsRecord;
+  tournament: TournamentWithFullDetailsRecord;
 };
 
 export const Bracket: React.FC<Props> = ({ tournament }) => {
-	const [matchesByRound, setMatchesByRound] = useState(
-		indexMatchesByRound(tournament.matches, tournament.swissRounds)
-	);
+  const [matchesByRound, setMatchesByRound] = useState(
+    indexMatchesByRound(tournament.matches, tournament.swissRounds)
+  );
 
-	useEffect(() => {
-		setMatchesByRound(indexMatchesByRound(tournament.matches, tournament.swissRounds));
-	}, [tournament]);
+  useEffect(() => {
+    setMatchesByRound(indexMatchesByRound(tournament.matches, tournament.swissRounds));
+  }, [tournament]);
 
-	const cls = useStyles();
+  const cls = useStyles();
 
-	useEffect(() => {
-		console.log('tournament in bracket : ', tournament);
-	}, [tournament]);
+  // useEffect(() => {
+  //   console.log('tournament in bracket : ', tournament);
+  // }, [tournament]);
 
-	return (
-		<div className={cls.container}>
-			{new Array(tournament.swissRounds).fill(null).map((_, i) => (
-				<div className={cls.roundContainer}>
-					<div>
-						<Text size="smallItalic">{`Round ${i + 1}`}</Text>
-					</div>
-					<div className={cls.round}>
-						{matchesByRound[i + 1] &&
-							matchesByRound[i + 1].length > 0 &&
-							matchesByRound[i + 1].map((match) => <MatchViewer match={match} />)}
-					</div>
-				</div>
-			))}
-		</div>
-	);
+  // console.log('matchesByRound', matchesByRound);
+  //
+  const rounds = useMemo(
+    () =>
+      range(
+        tournament.matches
+          .map((p) => p.round)
+          .reduce((prev, next) => (next > prev ? next : prev), 0)
+      ),
+    [tournament.matches.length]
+  );
+
+  return (
+    <div className={cls.container}>
+      {rounds.map((_, i) => (
+        <div key={`round-${i}`} className={cls.roundContainer}>
+          <div>
+            <Text size="smallItalic">{`Round ${i + 1}`}</Text>
+          </div>
+          <div className={cls.round}>
+            {matchesByRound[i + 1] &&
+              matchesByRound[i + 1].length > 0 &&
+              matchesByRound[i + 1].map((match) => <MatchViewer key={match.id} match={match} />)}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 const useStyles = createUseStyles({
-	container: {
-		display: 'flex',
-		flexDirection: 'column',
-		gap: spacers.default,
-		padding: spacers.default,
-	},
-	roundContainer: {
-		display: 'flex',
-		flexDirection: 'column',
-		gap: spacers.default,
-		marginBottom: spacers.large,
-	},
-	round: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		width: '100%',
-		gap: spacers.large,
-	},
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: spacers.default,
+    padding: spacers.default,
+  },
+  roundContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: spacers.default,
+    marginBottom: spacers.large,
+  },
+  round: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '100%',
+    gap: spacers.large,
+  },
 });
