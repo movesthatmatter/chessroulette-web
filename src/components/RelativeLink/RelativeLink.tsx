d5835b1529c340cb7a94d8f6727f249fac48cc4a
Gabriel C. Troia <catalin.troia@gmail.com>
2022-04-07 14:14:16 -0500

    feat(relativelink): take the underline out

--- src/components/RelativeLink/RelativeLink.tsx
+++ src/components/RelativeLink/RelativeLink.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react';
 import { Link, LinkProps, useRouteMatch } from 'react-router-dom';
+import { createUseStyles } from 'src/lib/jss';
 
 type Props = LinkProps;
 
@@ -7,8 +8,15 @@ const cleanUrl = (s: string) => s.replace(/([^:])(\/\/+)/g, '$1/');
 
 export const RelativeLink: React.FC<Props> = ({ to, ...props }) => {
   const { url } = useRouteMatch();
+  const cls = useStyles();
 
   const sanitizedPath = useMemo(() => cleanUrl(`${url}/${to}`), [url, to]);
 
-  return <Link to={sanitizedPath} {...props} />;
+  return <Link to={sanitizedPath} {...props} className={cls.a} />;
 };
+
+const useStyles = createUseStyles({
+  a: {
+    textDecoration: 'none !important',
+  },
+});
