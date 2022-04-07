660e145dafe82d63f2d9751b6f94a80474d2bb63
Gabriel C. Troia <catalin.troia@gmail.com>
2022-04-07 11:43:37 -0500

    feat(tournaments): add automatic play start for underway games

--- src/modules/Tournaments/TournamentMatchPage/TournamentMatchPage.tsx
+++ src/modules/Tournaments/TournamentMatchPage/TournamentMatchPage.tsx
@@ -1,21 +1,13 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { RegisteredUserRecord } from 'chessroulette-io';
-import { Button } from 'src/components/Button';
 import { Page } from 'src/components/Page';
 import { RelativeLink } from 'src/components/RelativeLink';
-import { createUseStyles } from 'src/lib/jss';
-import { JoinedRoomProvider } from 'src/modules/Room/Providers/JoinedRoomProvider';
-import { RoomConnectProvider } from 'src/modules/Room/Providers/RoomConnectProvider';
-import { ActivityRoomConsumer } from 'src/modules/Room/RoomConsumers/ActivityRoomConsumer';
-import { PeerToServerConsumer } from 'src/providers/PeerConnectionProvider';
 import { playTournamentMatch } from '../resources';
-import { GetRoomOrCreate } from '../room/GetRoomOrCreate';
-import { RoomBouncer } from '../room/RoomBouncer';
 import { TournamentMatchRecord } from '../types';
-import { console } from 'window-or-global';
 import { useResource } from 'src/lib/hooks/useResource';
-import { canJoinRoom } from 'src/modules/Room/resources';
 import { TournamentInProgressMatchPage } from '../TournamentInProgressMatchPage';
+import { AwesomeLoader } from 'src/components/AwesomeLoader';
+import { isDateInThePast } from 'src/modules/Room/util';
 
 type Props = {
   match: TournamentMatchRecord;
@@ -23,39 +15,43 @@ type Props = {
 };
 
 export const TournamentMatchPage: React.FC<Props> = ({ match: givenMatch, user }) => {
-  const cls = useStyles();
   const [match, setMatch] = useState(givenMatch);
-
-  const canJoinRoomResource = useResource(canJoinRoom);
+  const playTournamentMatchResource = useResource(playTournamentMatch);
 
   useEffect(() => {
     setMatch(givenMatch);
   }, [givenMatch]);
 
   const playMatch = useCallback(() => {
-    playTournamentMatch({
-      tournamentId: match.tournamentId,
-      matchId: match.id,
-    }).map((m) => {
-      console.log('setting the match', m);
+    if (playTournamentMatchResource.isLoading) {
+      return;
+    }
 
-      setMatch(m);
-    });
-  }, [match]);
+    playTournamentMatchResource
+      .request({
+        tournamentId: match.tournamentId,
+        matchId: match.id,
+      })
+      .map(setMatch);
+  }, [match, playTournamentMatchResource.isLoading]);
+
+  useEffect(() => {
+    if (match.state === 'underway' && isDateInThePast(match.underwayAt)) {
+      playMatch();
+    }
+  }, [match.state]);
 
   if (match.state === 'inProgress') {
-    return <TournamentInProgressMatchPage match={match}/>;
+    return <TournamentInProgressMatchPage match={match} />;
   }
 
   return (
     <Page name="Tournament Match">
       <pre>{JSON.stringify(match, null, 2)}</pre>
-      {match.state == 'underway' && <Button label="Play" onClick={playMatch} />}
+      {/* {match.state == 'underway' && <Button label="Play" onClick={playMatch} />} */}
       {match.state == 'complete' && <RelativeLink to="/analysis">Analyze</RelativeLink>}
+
+      {playTournamentMatchResource.isLoading && <AwesomeLoader />}
     </Page>
   );
 };
-
-const useStyles = createUseStyles({
-  container: {},
-});
